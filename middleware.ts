b719c6ac1852c9ef997d08aa1a54222ab6502572
tlmn <mail@tlmn.io>
2023-01-24 16:05:49 +0100

    set up basic auth

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
+
+const APP_USER = process.env.APP_USER;
+const APP_PASSWORD = process.env.APP_PASSWORD;
+
+if (!APP_USER || !APP_PASSWORD) {
+  throw new Error(
+    "APP_USER and APP_PASSWORD environment variables are required"
+  );
+}
+
+export const middleware = createNextAuthMiddleware({
+  users: [{ name: APP_USER, password: APP_PASSWORD }],
+});
+
+export const config = {
+  matcher: ["/src/(.*)", "/_next/static/chunks/(.*)"],
+};
