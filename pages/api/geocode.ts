721905c2ca03065305e16eb74e0c82010efba809
tlmn <mail@tlmn.io>
2023-01-25 15:43:23 +0100

    add geocode api

---
+++ pages/api/geocode.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as NodeGeocoder from "node-geocoder";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.query.address === undefined) {
+    res.status(400).json({ error: "address is required" });
+  }
+
+  const options: NodeGeocoder.Options = {
+    provider: "google",
+    apiKey: process.env.GOOGLE_MAPS_API_KEY,
+  };
+  
+  // @ts-ignore
+  const geocoder = NodeGeocoder(options);
+
+  const results = await geocoder.geocode(req.query.address);
+
+  res.status(200).json(results);
+};
+
+export default handler;
