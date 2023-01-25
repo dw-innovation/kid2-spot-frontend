2926cdf120d19cd6e5948aec62203112a6b1e3fb
tlmn <mail@tlmn.io>
2023-01-25 10:57:35 +0100

    use mapZoom

--- src/components/map/MapContainer.tsx
+++ src/components/map/MapContainer.tsx
@@ -5,9 +5,10 @@ import Markers from "./Markers";
 
 const MapContainer = () => {
   const mapCenter = useSessionStore((state) => state.mapCenter);
+  const mapZoom = useSessionStore((state) => state.mapZoom);
   return (
     // @ts-ignore
-    <LeafletMapContainer center={mapCenter} zoom={13} scrollWheelZoom={false}>
+    <LeafletMapContainer center={mapCenter} zoom={mapZoom} scrollWheelZoom={false}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
