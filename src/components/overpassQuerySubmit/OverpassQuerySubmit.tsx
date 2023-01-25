56f7a050cd2274ee5eafb036f507c65b32409132
tlmn <mail@tlmn.io>
2023-01-25 11:11:32 +0100

    style

--- src/components/overpassQuerySubmit/OverpassQuerySubmit.tsx
+++ src/components/overpassQuerySubmit/OverpassQuerySubmit.tsx
@@ -3,13 +3,19 @@ import { callOverpassAPI } from "@/lib/utils";
 import useSessionStore from "src/stores/useSessionStore";
 import { RotatingLines } from "react-loader-spinner";
 import Triangle from "src/assets/icons/triangle";
+import clsx from "clsx";
 
 const OverpassQuerySubmit = () => {
   const apiState = useSessionStore((state) => state.apiState);
   return (
     <button
       onClick={() => callOverpassAPI()}
-      className="block bg-slate-100 px-2 py-1 hover:bg-slate-300"
+      className={clsx(
+        "block  px-2 py-1",
+        apiState === "idle" && "bg-slate-100 hover:bg-slate-300",
+        apiState === "loading" && "bg-slate-100",
+        apiState === "error" && "bg-red-100",
+      )}
       disabled={apiState === "loading"}
     >
       <div className="flex gap-2 items-center">
