0c8ee8ff9d5f546ab9871548b6f4f5945a13351e
tlmn <mail@tlmn.io>
2023-01-25 10:01:11 +0100

    add loading spinner

--- src/components/overpassQuerySubmit/OverpassQuerySubmit.tsx
+++ src/components/overpassQuerySubmit/OverpassQuerySubmit.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import { callOverpassAPI } from "@/lib/utils";
+import useSessionStore from "src/stores/useSessionStore";
+import { RotatingLines } from "react-loader-spinner";
 
 const OverpassQuerySubmit = () => {
-  return <button onClick={() => callOverpassAPI()} className="block bg-slate-100 px-2 py-1 hover:bg-slate-300">run query</button>;
+  const apiState = useSessionStore((state) => state.apiState);
+  return (
+    <button
+      onClick={() => callOverpassAPI()}
+      className="block bg-slate-100 px-2 py-1 hover:bg-slate-300"
+      disabled={apiState === "loading"}
+    >
+      {apiState === "loading" ? (
+        <div className="flex gap-2 items-center">
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="20"
+          visible={true}
+        />
+        running query
+        </div>
+      ) : (
+        "run query"
+      )}
+    </button>
+  );
 };
 
 export default OverpassQuerySubmit;
