6b6e5fd3cc3ab24c9987e07eff06927431061b5e
tlmn <mail@tlmn.io>
2023-01-25 10:57:11 +0100

    add marker

--- src/components/map/Markers.tsx
+++ src/components/map/Markers.tsx
@@ -1,15 +1,20 @@
 import React, { Fragment } from "react";
 import { Marker, Popup } from "react-leaflet";
+import * as L from "leaflet";
 import useSessionStore from "src/stores/useSessionStore";
 
 const Markers = () => {
   const markers = useSessionStore((state) => state.markers);
+  const icon = L.icon({
+    iconUrl: "/images/marker-icon.png",
+    iconSize: [20, 30],
+  });
   return (
     <>
       {markers.length > 0 &&
         markers.map(({ lat, lon, ...item }, index) => (
           <Fragment key={index}>
-            <Marker position={[lat, lon]}>
+            <Marker position={[lat, lon]} icon={icon}>
               <Popup>
                 <pre>{JSON.stringify(item, null, 2)}</pre>
               </Popup>
