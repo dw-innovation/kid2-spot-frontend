48b62749c4ddad302a60351609a59f3358ec0e7d
tlmn <mail@tlmn.io>
2023-01-25 11:09:02 +0100

    add triangle

--- src/components/overpassQuerySubmit/OverpassQuerySubmit.tsx
+++ src/components/overpassQuerySubmit/OverpassQuerySubmit.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { callOverpassAPI } from "@/lib/utils";
 import useSessionStore from "src/stores/useSessionStore";
 import { RotatingLines } from "react-loader-spinner";
+import Triangle from "src/assets/icons/triangle";
 
 const OverpassQuerySubmit = () => {
   const apiState = useSessionStore((state) => state.apiState);
@@ -11,20 +12,27 @@ const OverpassQuerySubmit = () => {
       className="block bg-slate-100 px-2 py-1 hover:bg-slate-300"
       disabled={apiState === "loading"}
     >
-      {apiState === "loading" ? (
-        <div className="flex gap-2 items-center">
-        <RotatingLines
-          strokeColor="grey"
-          strokeWidth="5"
-          animationDuration="0.75"
-          width="20"
-          visible={true}
-        />
-        running query
-        </div>
-      ) : (
-        "run query"
-      )}
+      <div className="flex gap-2 items-center">
+        {apiState === "loading" ? (
+          <>
+            <RotatingLines
+              strokeColor="grey"
+              strokeWidth="5"
+              animationDuration="0.75"
+              width="20"
+              visible={true}
+            />
+            running query
+          </>
+        ) : (
+          <>
+            <span className="text-green-600">
+              <Triangle width={20} />
+            </span>{" "}
+            run query
+          </>
+        )}
+      </div>
     </button>
   );
 };
