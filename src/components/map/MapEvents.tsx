8084b0b31e79a16affb735d6147c696fedf27a46
tlmn <mail@tlmn.io>
2023-03-20 14:21:49 +0100

    use key hooks

--- src/components/map/MapEvents.tsx
+++ src/components/map/MapEvents.tsx
@@ -1,6 +1,7 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useMap } from "react-leaflet";
 
+import useKeyPress from "@/lib/hooks/useKeyPress";
 import { getFlyToAnimationSpeed } from "@/lib/utils";
 import useMapStore from "@/stores/useMapStore";
 
@@ -8,6 +9,10 @@ const MapEvents = () => {
   const setBbox = useMapStore((state) => state.setBbox);
   const setMapZoom = useMapStore((state) => state.setMapZoom);
   const mapCenter = useMapStore((state) => state.mapCenter);
+  const togglePolygonMode = useMapStore((state) => state.togglePolygonMode);
+  const clearPolygon = useMapStore((state) => state.clearPolygon);
+
+  const [isMouseOver, setIsMouseOver] = useState(false);
 
   const map = useMap();
 
@@ -24,26 +29,41 @@ const MapEvents = () => {
     setMapZoom(map.getZoom());
   }, [map, setMapZoom]);
 
+  const handleMouseOver = useCallback(() => {
+    setIsMouseOver(true);
+  }, []);
+
+  const handleMouseOut = useCallback(() => {
+    setIsMouseOver(false);
+  }, []);
+
   useEffect(() => {
     updateZoom();
     updateBbox();
     map.on("moveend", () => updateBbox());
     map.on("zoomlevelschange", () => updateZoom());
+    map.on("mouseover", () => handleMouseOver());
+    map.on("mouseout", () => handleMouseOut());
 
     return () => {
       map.off("moveend", () => updateBbox());
       map.off("zoomlevelschange", () => updateZoom());
+      map.off("mouseover", () => handleMouseOver());
+      map.off("mouseout", () => handleMouseOut());
     };
-  }, [map, setBbox, updateBbox, updateZoom]);
+  }, [handleMouseOut, handleMouseOver, map, setBbox, updateBbox, updateZoom]);
 
-  useEffect(() => {
+  /*   useEffect(() => {
     // @ts-ignore
     map.flyTo(mapCenter, undefined, {
       animate: true,
       // @ts-ignore
       duration: getFlyToAnimationSpeed(map.getCenter(), mapCenter),
     });
-  }, [mapCenter, map]);
+  }, [mapCenter, map]); */
+
+  useKeyPress("p", () => isMouseOver && togglePolygonMode());
+  useKeyPress("c", () => isMouseOver && clearPolygon());
 
   return <></>;
 };
