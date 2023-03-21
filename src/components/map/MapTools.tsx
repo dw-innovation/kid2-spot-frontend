a3a29e4e635323ef9b410340882d8b1ac8f7ac92
tlmn <mail@tlmn.io>
2023-03-21 15:31:01 +0100

    add clear button

--- src/components/map/MapTools.tsx
+++ src/components/map/MapTools.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { useMap } from "react-leaflet";
 
 import MinusIcon from "@/assets/icons/MinusIcon";
 import PlusIcon from "@/assets/icons/PlusIcon";
 import PolygonIcon from "@/assets/icons/PolygonIcon";
+import PolygonTrashIcon from "@/assets/icons/PolygonTrashIcon";
 import useMapStore from "@/stores/useMapStore";
+import usePolygonStore from "@/stores/usePolygonStore";
 
 import MapButton from "../MapButton";
 
@@ -12,6 +14,8 @@ const MapTools = () => {
   const map = useMap();
   const togglePolygonMode = useMapStore((state) => state.togglePolygonMode);
   const polygonMode = useMapStore((state) => state.polygonMode);
+  const polygon = usePolygonStore((state) => state.polygon);
+  const clearPolygon = usePolygonStore((state) => state.clearPolygon);
 
   return (
     <div
@@ -27,6 +31,11 @@ const MapTools = () => {
       <MapButton onClick={() => togglePolygonMode()} isActive={polygonMode}>
         <PolygonIcon size={20} />
       </MapButton>
+      {polygon.length > 0 && (
+        <MapButton onClick={() => clearPolygon()}>
+          <PolygonTrashIcon size={20} />
+        </MapButton>
+      )}
     </div>
   );
 };
