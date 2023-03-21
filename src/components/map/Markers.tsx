274b7d74aefc8f6340cb920ef08da3a5d4b59da7
tlmn <mail@tlmn.io>
2023-03-21 15:47:26 +0100

    do not render marker when lat or lon is undefined

--- src/components/map/Markers.tsx
+++ src/components/map/Markers.tsx
@@ -13,22 +13,24 @@ const Markers = () => {
   return (
     <>
       {markers.length > 0 &&
-        markers.map(({ lat, lon, ...item }, index) => (
-          <Fragment key={index}>
-            <Marker position={[lat, lon]} icon={icon}>
-              <Popup>
-                <pre>{JSON.stringify(item, null, 2)}</pre>
-                <a
-                  href={`https://maps.google.com/maps?q=&layer=c&cbll=${lat},${lon}&cbp=11,0,0,0,0`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  view on Google Street View
-                </a>
-              </Popup>
-            </Marker>
-          </Fragment>
-        ))}
+        markers.map(({ lat, lon, ...item }, index) =>
+          lat === undefined || lon === undefined ? null : (
+            <Fragment key={index}>
+              <Marker position={new L.LatLng(lat, lon)} icon={icon}>
+                <Popup>
+                  <pre>{JSON.stringify(item, null, 2)}</pre>
+                  <a
+                    href={`https://maps.google.com/maps?q=&layer=c&cbll=${lat},${lon}&cbp=11,0,0,0,0`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    view on Google Street View
+                  </a>
+                </Popup>
+              </Marker>
+            </Fragment>
+          )
+        )}
     </>
   );
 };
