1290eba3e0f020983ac2e1e9923728b1e7bd4351
tlmn <mail@tlmn.io>
2023-03-22 16:11:18 +0100

    use divIcon

--- src/components/map/Markers.tsx
+++ src/components/map/Markers.tsx
@@ -1,14 +1,17 @@
 import * as L from "leaflet";
-import React, { Fragment } from "react";
-import { FeatureGroup, LayerGroup, Marker, Popup } from "react-leaflet";
+import React, { Fragment, useEffect } from "react";
+import { FeatureGroup, LayerGroup, Marker, Popup, useMap } from "react-leaflet";
 
 import useMapStore from "@/stores/useMapStore";
 
 const Markers = () => {
   const markers = useMapStore((state) => state.markers);
-  const icon = L.icon({
-    iconUrl: "/images/marker-icon.png",
-    iconSize: [20, 30],
+
+  const icon = L.divIcon({
+    className: "result-marker-icon",
+    html: `<div style="width: 15px; height: 15px; background-color: rgba(255, 255, 255, 0.1); border: 2px grey solid; border-radius:9000px"></div>`,
+    iconSize: [10, 10],
+    iconAnchor: [5, 5],
   });
 
   return (
