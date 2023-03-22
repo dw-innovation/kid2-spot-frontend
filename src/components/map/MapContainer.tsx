7800d4f744e08108f498517594c4c973500779f7
tlmn <mail@tlmn.io>
2023-03-22 16:19:25 +0100

    Close polygon mode when clicking outside the map

--- src/components/map/MapContainer.tsx
+++ src/components/map/MapContainer.tsx
@@ -1,7 +1,9 @@
+import { useRef } from "react";
 import { MapContainer as LeafletMapContainer, TileLayer } from "react-leaflet";
 import VectorTileLayer from "react-leaflet-vector-tile-layer";
 
 import AddressSearchBox from "@/components/addressSearch";
+import useOnClickOutside from "@/lib/hooks/useOnClickOutside";
 import useMapStore from "@/stores/useMapStore";
 
 import MapEvents from "./MapEvents";
@@ -11,40 +13,46 @@ import Markers from "./Markers";
 import PolygonDrawer from "./polygon/PolygonDrawer";
 
 const MapContainer = () => {
+  const mapContainerRef = useRef(null);
   const mapCenter = useMapStore((state) => state.mapCenter);
   const mapZoom = useMapStore((state) => state.mapZoom);
   const tilesServer = useMapStore((state) => state.tilesServer);
+  const togglePolygonMode = useMapStore((state) => state.togglePolygonMode);
+
+  useOnClickOutside(mapContainerRef, () => togglePolygonMode(false)); // Close polygon mode when clicking outside the map
+
   return (
     <>
       <div className="absolute top-0 right-0 z-[9999] mt-2 mr-2 flex">
         <AddressSearchBox />
       </div>
+      <div ref={mapContainerRef} className="w-full h-full">
+        <LeafletMapContainer
+          center={mapCenter}
+          zoom={mapZoom}
+          scrollWheelZoom={true}
+          className="cursor-crosshair"
+          zoomControl={false}
+        >
+          {tilesServer === "osm" && (
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+          )}
+          {tilesServer === "vector" && (
+            <VectorTileLayer
+              styleUrl={`https://api.maptiler.com/maps/basic-v2/style.json?key=${process.env.NEXT_PUBLIC_MAPTILER_KEY}`}
+            />
+          )}
 
-      <LeafletMapContainer
-        center={mapCenter}
-        zoom={mapZoom}
-        scrollWheelZoom={true}
-        className="cursor-crosshair"
-        zoomControl={false}
-      >
-        {tilesServer === "osm" && (
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-        )}
-        {tilesServer === "vector" && (
-          <VectorTileLayer
-            styleUrl={`https://api.maptiler.com/maps/basic-v2/style.json?key=${process.env.NEXT_PUBLIC_MAPTILER_KEY}`}
-          />
-        )}
-
-        <MapTools />
-        <MapEvents />
-        <MapKeyEvents />
-        <PolygonDrawer />
-        <Markers />
-      </LeafletMapContainer>
+          <MapTools />
+          <MapEvents />
+          <MapKeyEvents />
+          <PolygonDrawer />
+          <Markers />
+        </LeafletMapContainer>
+      </div>
     </>
   );
 };
