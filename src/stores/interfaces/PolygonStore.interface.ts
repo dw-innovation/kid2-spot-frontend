1a02fdeaaed88f79c1901cecccd688dcff3fdc61
tlmn <mail@tlmn.io>
2023-03-24 09:06:41 +0100

    add clearPolygon()

--- src/stores/interfaces/PolygonStore.interface.ts
+++ src/stores/interfaces/PolygonStore.interface.ts
@@ -1,6 +1,7 @@
 export default interface PolygonStoreInterface {
   polygon: [number, number][];
   setPolygon: (polygon: [number, number][]) => void;
+  clearPolygon: () => void;
   polygonOutsideBBox: boolean;
   togglePolygonOutsideBBox: (state?: boolean) => void;
 }
