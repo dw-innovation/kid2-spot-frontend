6c2d4070417f1aadd60aeb08359c660d60923253
tlmn <mail@tlmn.io>
2023-03-31 14:02:25 +0200

    load allotment styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
+import "allotment/dist/style.css";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
