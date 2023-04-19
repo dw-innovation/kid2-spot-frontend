4493f375c7cda1776c2816b6eec8bf9daf18773d
tlmn <mail@tlmn.io>
2023-04-19 16:29:56 +0200

    simplify default query

--- src/stores/useQueryStore.ts
+++ src/stores/useQueryStore.ts
@@ -5,7 +5,7 @@ import QueryStoreInterface from "./interfaces/QueryStore.interface";
 
 const useAppStore = create<QueryStoreInterface>((set) => ({
   overpassQuery:
-    '// find all cafés that are no more than 200m from a subway entrance\n\n\n[out:json][timeout:250];\n\n(\n  nwr["railway"="subway_entrance"]({{AREA}})->.subwayentrances;\n  \n  nwr["amenity"="cafe"](around.subwayentrances:200)->.cafes;\n);\n\nout body;\n>;\nout skel qt;',
+    '// find all cafés that are no more than 200m from a subway entrance\n\n[out:json][timeout:250];\n\narea[name="Berlin"]->.berlin;\n(\n    nwr(area.berlin)["railway"="subway_entrance"]->.subway_entrances;\n    nwr(around.subway_entrances:200)["amenity"="cafe"]->.cafes;\n);\n\n.cafes out body;',
   setOverpassQuery: (overpassQuery: string) => {
     set(
       produce((draft) => {
