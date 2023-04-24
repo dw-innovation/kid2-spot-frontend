b8a0a84a85623489c6354f08985828391d4e89e0
tlmn <mail@tlmn.io>
2023-04-24 09:17:04 +0200

    add close feature

--- src/components/map/polygon/PolygonOutsideAlert.tsx
+++ src/components/map/polygon/PolygonOutsideAlert.tsx
@@ -1,6 +1,7 @@
 import { bbox } from "@turf/turf";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import PlusIcon from "@/assets/icons/PlusIcon";
 import useMapStore from "@/stores/useMapStore";
 import usePolygonStore from "@/stores/usePolygonStore";
 
@@ -10,6 +11,8 @@ const PolygonOutsideAlert = () => {
   );
   const polygon = usePolygonStore((state) => state.polygon);
   const setBounds = useMapStore((state) => state.setBounds);
+
+  const [showAlert, setShowAlert] = useState(false);
 
   const handleFlyToBounds = () => {
     let newBBox = bbox({
@@ -22,16 +25,30 @@ const PolygonOutsideAlert = () => {
     ]);
   };
 
+  useEffect(() => {
+    setShowAlert(polygonOutsideBBox);
+  }, [polygonOutsideBBox]);
+
+  const handleCloseClick = () => {
+    setShowAlert(false);
+  };
+
   return (
     <>
-      {polygonOutsideBBox && (
-        <span className="flex items-center justify-center gap-2 px-2 py-1 mr-2 text-black bg-orange-400 rounded-lg shadow-lg">
+      {polygonOutsideBBox && showAlert && (
+        <span className="relative flex items-center justify-center gap-2 px-2 py-1 mr-2 text-black bg-orange-400 rounded-lg shadow-lg">
           Polygon outside bounding box!
           <button
             onClick={handleFlyToBounds}
             className="p-1 bg-white rounded-lg hover:bg-slate-200"
           >
             fly to polygon
+          </button>
+          <button
+            className="absolute top-0 right-0 rotate-45 bg-white rounded-full translate-x-1/3 -translate-y-1/3 hover:bg-slate-200"
+            onClick={handleCloseClick}
+          >
+            <PlusIcon />
           </button>
         </span>
       )}
