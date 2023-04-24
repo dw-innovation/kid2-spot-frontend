51fa0c2aa890a0ebaf443e697ce767bca0b09b91
tlmn <mail@tlmn.io>
2023-04-24 13:00:52 +0200

    fix unused imports

--- src/components/OverpassEditor/overpass-mode.js
+++ src/components/OverpassEditor/overpass-mode.js
@@ -1,9 +1,8 @@
 import ace from "ace-builds/src-noconflict/ace";
 
-ace.define("ace/mode/overpass", function (require, exports, module) {
+ace.define("ace/mode/overpass", function (require, exports) {
   const oop = require("ace/lib/oop");
   const TextMode = require("ace/mode/text").Mode;
-  const Tokenizer = require("ace/tokenizer").Tokenizer;
   const OverpassHighlightRules =
     require("ace/mode/overpass_highlight_rules").OverpassHighlightRules;
 
@@ -15,103 +14,100 @@ ace.define("ace/mode/overpass", function (require, exports, module) {
 
   (function () {
     this.$id = "ace/mode/overpass";
-  }.call(Mode.prototype));
+  }).call(Mode.prototype);
 
   exports.Mode = Mode;
 });
 
-ace.define(
-  "ace/mode/overpass_highlight_rules",
-  function (require, exports, module) {
-    const oop = require("ace/lib/oop");
-    const TextHighlightRules =
-      require("ace/mode/text_highlight_rules").TextHighlightRules;
+ace.define("ace/mode/overpass_highlight_rules", function (require, exports) {
+  const oop = require("ace/lib/oop");
+  const TextHighlightRules =
+    require("ace/mode/text_highlight_rules").TextHighlightRules;
 
-    const OverpassHighlightRules = function () {
-      this.$rules = {
-        start: [
-          {
-            token: "keyword.operator",
-            regex: /\[.*?\]/,
-          },
-          {
-            token: "keyword",
-            regex:
-              /\b(node|way|relation|nwr|area|out|meta|body|tags|qt|asc|desc|skip|limit|bbox|poly|changed|newer|if|else|foreach|count|set|for|difference|break|continue|return)\b/,
-          },
-          {
-            token: "variable",
-            regex: /[@_]\w+/,
-          },
-          {
-            token: "constant.numeric",
-            regex: /-?\d+(\.\d+)?/,
-          },
-          {
-            token: "constant.numeric",
-            regex: /{{AREA}}/,
-          },
-          {
-            token: "string",
-            regex: /'(?:\\.|[^'])*'/,
-          },
-          {
-            token: "string",
-            regex: /"(?:\\.|[^"])*"/,
-          },
-          {
-            token: "paren.lparen",
-            regex: /[\[{(]/,
-          },
-          {
-            token: "paren.rparen",
-            regex: /[\]})]/,
-          },
-          {
-            token: "comment",
-            regex: /\/\/.*/,
-            next: "singleLineComment",
-          },
-          {
-            token: "comment",
-            regex: /#.*/,
-            next: "singleLineComment",
-          },
-          {
-            token: "comment.start",
-            regex: /\/\*/,
-            next: "multiLineComment",
-          },
-          {
-            token: "support.function",
-            regex: /\b\w+(?=\()/,
-          },
-        ],
-        singleLineComment: [
-          {
-            token: "comment",
-            regex: /$/,
-            next: "start",
-          },
-          {
-            defaultToken: "comment",
-          },
-        ],
-        multiLineComment: [
-          {
-            token: "comment.end",
-            regex: /\*\//,
-            next: "start",
-          },
-          {
-            defaultToken: "comment",
-          },
-        ],
-      };
+  const OverpassHighlightRules = function () {
+    this.$rules = {
+      start: [
+        {
+          token: "keyword.operator",
+          regex: /\[.*?\]/,
+        },
+        {
+          token: "keyword",
+          regex:
+            /\b(node|way|relation|nwr|area|out|meta|body|tags|qt|asc|desc|skip|limit|bbox|poly|changed|newer|if|else|foreach|count|set|for|difference|break|continue|return)\b/,
+        },
+        {
+          token: "variable",
+          regex: /[@_]\w+/,
+        },
+        {
+          token: "constant.numeric",
+          regex: /-?\d+(\.\d+)?/,
+        },
+        {
+          token: "constant.numeric",
+          regex: /{{AREA}}/,
+        },
+        {
+          token: "string",
+          regex: /'(?:\\.|[^'])*'/,
+        },
+        {
+          token: "string",
+          regex: /"(?:\\.|[^"])*"/,
+        },
+        {
+          token: "paren.lparen",
+          regex: /[\[{(]/,
+        },
+        {
+          token: "paren.rparen",
+          regex: /[\]})]/,
+        },
+        {
+          token: "comment",
+          regex: /\/\/.*/,
+          next: "singleLineComment",
+        },
+        {
+          token: "comment",
+          regex: /#.*/,
+          next: "singleLineComment",
+        },
+        {
+          token: "comment.start",
+          regex: /\/\*/,
+          next: "multiLineComment",
+        },
+        {
+          token: "support.function",
+          regex: /\b\w+(?=\()/,
+        },
+      ],
+      singleLineComment: [
+        {
+          token: "comment",
+          regex: /$/,
+          next: "start",
+        },
+        {
+          defaultToken: "comment",
+        },
+      ],
+      multiLineComment: [
+        {
+          token: "comment.end",
+          regex: /\*\//,
+          next: "start",
+        },
+        {
+          defaultToken: "comment",
+        },
+      ],
     };
+  };
 
-    oop.inherits(OverpassHighlightRules, TextHighlightRules);
+  oop.inherits(OverpassHighlightRules, TextHighlightRules);
 
-    exports.OverpassHighlightRules = OverpassHighlightRules;
-  }
-);
+  exports.OverpassHighlightRules = OverpassHighlightRules;
+});
