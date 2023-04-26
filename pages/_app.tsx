48c3281c129504ac433f7799dacb117e9f109494
tlmn <mail@tlmn.io>
2023-04-26 17:05:23 +0200

    render toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,11 @@ import "@/styles/globals.css";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 import "allotment/dist/style.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { ToastContainer } from "react-toastify";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
@@ -13,6 +15,18 @@ const App = ({ Component, pageProps }: AppProps) => (
       <meta name="description" content="KID2 Overpass Turbo Prototype" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
     <Component {...pageProps} />
   </>
 );
