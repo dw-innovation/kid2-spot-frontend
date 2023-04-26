562a0ce2f25ec79a2cdde4510abb3a7a067c3c13
tlmn <mail@tlmn.io>
2023-04-26 13:58:27 +0200

    style

--- src/components/map/FeatureInfo.tsx
+++ src/components/map/FeatureInfo.tsx
@@ -4,21 +4,34 @@ type Props = {
   feature: any;
 };
 
-const FeatureInfo = ({ feature }: Props) => {
-  return (
-    <div>
+const FeatureInfo = ({ feature }: Props) => (
+  <div>
+    <div className="pb-2">
       {feature.properties.name && (
         <h3 className="text-lg font-bold leading-tight">
           {feature.properties.name}
         </h3>
       )}
-      {Object.keys(feature.properties).map((key, index) => (
-        <span key={index} className="block">
-          <strong>{key}</strong>: {feature.properties[key]}
-        </span>
-      ))}
+      {feature.properties.id && (
+        <a
+          href={`https://www.openstreetmap.org/${feature.properties.id}`}
+          target="_blank"
+          rel="noreferrer"
+          className="block"
+        >
+          {feature.properties.id}
+        </a>
+      )}
     </div>
-  );
-};
+    <span className="text-lg font-bold uppercase">
+      Tags ({Object.keys(feature.properties).length})
+    </span>
+    {Object.keys(feature.properties).map((key, index) => (
+      <span key={index} className="block">
+        <strong>{key}</strong>: {feature.properties[key]}
+      </span>
+    ))}
+  </div>
+);
 
 export default FeatureInfo;
