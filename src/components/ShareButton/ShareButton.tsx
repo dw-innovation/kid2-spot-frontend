15ddc070e78275693e6ae4ef5a6b924910c0e9e7
tlmn <mail@tlmn.io>
2023-04-26 17:05:40 +0200

    add share button

---
+++ src/components/ShareButton/ShareButton.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { saveData } from "@/lib/storeData";
+import useMapStore from "@/stores/useMapStore";
+import useQueryStore from "@/stores/useQueryStore";
+
+import Button from "../Button";
+
+const ShareButton = () => {
+  return (
+    <Button
+      onClick={() =>
+        saveData([
+          { name: "useMapStore", getState: useMapStore.getState },
+          { name: "useQueryStore", getState: useQueryStore.getState },
+        ])
+      }
+    >
+      share session
+    </Button>
+  );
+};
+
+export default ShareButton;
