b0603c03244d1d575d4d36577f1b3ffb0e1d6e5f
tlmn <mail@tlmn.io>
2023-04-27 11:15:40 +0200

    fix value not showing

--- src/components/ResultsEditor/ResultsEditor.tsx
+++ src/components/ResultsEditor/ResultsEditor.tsx
@@ -3,36 +3,40 @@ import "ace-builds/src-noconflict/theme-xcode";
 import "ace-builds/src-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/mode-json";
 
-import { FeatureCollection } from "geojson";
 import React, { useEffect } from "react";
 import AceEditor from "react-ace";
 
 import useResultsStore from "@/stores/useResultsStore";
 
 const ResultsEditor = () => {
-  const [editorData, setEditorData] =
-    React.useState<null | FeatureCollection>();
+  const editorRef = React.useRef<AceEditor>(null);
   const geoJSONResults = useResultsStore((state) => state.geoJSON);
 
   useEffect(() => {
-    setEditorData(geoJSONResults);
+    if (editorRef.current) {
+      editorRef.current.editor.setValue(
+        JSON.stringify(geoJSONResults, null, 2)
+      );
+    }
   }, [geoJSONResults]);
 
   return (
-    <AceEditor
-      value={JSON.stringify(editorData, null, 2)}
-      mode="json"
-      theme="xcode"
-      name="geojson-results"
-      editorProps={{ $blockScrolling: true }}
-      setOptions={{
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-      }}
-      width="100%"
-      height="100%"
-      readOnly
-    />
+    <>
+      <AceEditor
+        ref={editorRef}
+        mode="json"
+        theme="xcode"
+        name="geojson-results"
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+        }}
+        width="100%"
+        height="100%"
+        readOnly
+      />
+    </>
   );
 };
 
