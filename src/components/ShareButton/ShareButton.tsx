4e0150836f95e6b6b418e7ab7d000b0a8b3eea35
tlmn <mail@tlmn.io>
2023-04-28 15:20:05 +0200

    add loading spinner

--- src/components/ShareButton/ShareButton.tsx
+++ src/components/ShareButton/ShareButton.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { RotatingLines } from "react-loader-spinner";
 
+import useApiStatus from "@/lib/hooks/useApiStatus";
 import { saveData } from "@/lib/storeData";
 import useAppStore from "@/stores/useAppStore";
 import useMapStore from "@/stores/useMapStore";
@@ -7,18 +9,32 @@ import useQueryStore from "@/stores/useQueryStore";
 
 import Button from "../Button";
 
-const ShareButton = () => (
-  <Button
-    onClick={() =>
-      saveData([
-        { name: "useAppStore", getState: useAppStore.getState },
-        { name: "useMapStore", getState: useMapStore.getState },
-        { name: "useQueryStore", getState: useQueryStore.getState },
-      ])
-    }
-  >
-    share session
-  </Button>
-);
+const ShareButton = () => {
+  const [apiStatus, triggerSaveData] = useApiStatus(() =>
+    saveData([
+      { name: "useAppStore", getState: useAppStore.getState },
+      { name: "useMapStore", getState: useMapStore.getState },
+      { name: "useQueryStore", getState: useQueryStore.getState },
+    ])
+  );
+
+  return (
+    <Button
+      onClick={() => triggerSaveData()}
+      className="flex items-center gap-2"
+    >
+      share session
+      {apiStatus === "loading" && (
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="20"
+          visible={true}
+        />
+      )}
+    </Button>
+  );
+};
 
 export default ShareButton;
