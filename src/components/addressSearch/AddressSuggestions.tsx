757fb8ac5b29174f7f55ffdee427d5349f7edac3
tlmn <mail@tlmn.io>
2023-05-04 12:25:51 +0200

    handle select event

--- src/components/addressSearch/AddressSuggestions.tsx
+++ src/components/addressSearch/AddressSuggestions.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
-import React from "react";
+import { Callback, StateChangeOptions } from "downshift";
+import React, { useRef } from "react";
 
 interface AddressSuggestionProps {
   isOpen: boolean;
@@ -7,6 +8,11 @@ interface AddressSuggestionProps {
   highlightedIndex: number | null;
   selectedItem: any;
   getItemProps: (options: any) => any;
+  selectItem: (
+    item: any,
+    otherStateToSet?: Partial<StateChangeOptions<any>> | undefined,
+    cb?: Callback | undefined
+  ) => void;
 }
 
 const AddressSuggestions = ({
@@ -15,30 +21,41 @@ const AddressSuggestions = ({
   highlightedIndex,
   selectedItem,
   getItemProps,
-}: AddressSuggestionProps) => (
-  <ul
-    className={`absolute w-full bg-white mt-1 shadow-md max-h-80 p-0 ${
-      !isOpen && suggestions.length === 0 && "hidden"
-    }`}
-  >
-    {isOpen &&
-      suggestions.map((item, index) => (
-        <li
-          key={index}
-          className={clsx(
-            highlightedIndex === index && "bg-blue-300",
-            selectedItem === item && "font-bold",
-            "py-2 px-3 shadow-sm flex flex-col cursor-pointer"
-          )}
-          {...getItemProps({
-            index,
-            item,
-          })}
-        >
-          <span>{item.place_name}</span>
-        </li>
-      ))}
-  </ul>
-);
+  selectItem,
+}: AddressSuggestionProps) => {
+  const suggestionsRef = useRef(null);
+
+  const handleClickSelect = (item: any) => {
+    selectItem(item);
+  };
+
+  return (
+    <ul
+      className={`absolute w-full bg-white mt-1 shadow-md max-h-80 p-0 ${
+        !isOpen && suggestions.length === 0 && "hidden"
+      }`}
+      ref={suggestionsRef}
+    >
+      {isOpen &&
+        suggestions.map((item, index) => (
+          <li
+            key={index}
+            className={clsx(
+              highlightedIndex === index && "bg-blue-300",
+              selectedItem === item && "font-bold",
+              "py-2 px-3 shadow-sm flex flex-col cursor-pointer"
+            )}
+            {...getItemProps({
+              index,
+              item,
+            })}
+            onMouseDown={() => handleClickSelect(item)}
+          >
+            <span>{item.place_name}</span>
+          </li>
+        ))}
+    </ul>
+  );
+};
 
 export default AddressSuggestions;
