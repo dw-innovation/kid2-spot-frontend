f54b3d42508e9b549a74d31d88e9b05737890d5b
tlmn <mail@tlmn.io>
2023-08-20 19:15:05 +0200

    remove left pane

--- src/components/Interface/Interface.tsx
+++ src/components/Interface/Interface.tsx
@@ -5,53 +5,24 @@ import clsx from "clsx";
 import React from "react";
 
 import DynamicMap from "@/components/map";
-import NaturalLanguageInput from "@/components/NaturalLanguageInput";
-import NatualLanguageSubmitButton from "@/components/NaturalLanguageInput/NatualLanguageSubmitButton";
-import OSMQuerySubmit from "@/components/OSMQuerySubmit";
-import QueryAreaSelector from "@/components/QueryAreaSelector";
 import DynamicResultsViewer from "@/components/ResultsViewer";
 import StreetViewPane from "@/components/StreetViewPane";
 import useAppStore from "@/stores/useAppStore";
 import useStreetViewStore from "@/stores/useStreetViewStore";
 
 import Header from "../Header";
-import QuerySets from "../QuerySets/QuerySets";
 
 const Interface = () => {
   const view = useAppStore((state) => state.view);
   const showStreetView = useStreetViewStore((state) => state.showStreetView);
+
   return (
     <>
       <div className="pb-3">
         <Header />
       </div>
-      <Allotment defaultSizes={[30, 70]}>
-        <Allotment.Pane className="pr-1">
-          <Allotment vertical defaultSizes={[20, 80]}>
-            <Allotment.Pane className="pb-2">
-              <div className="flex flex-col h-full gap-1">
-                <NaturalLanguageInput />
-                <NatualLanguageSubmitButton />
-              </div>
-            </Allotment.Pane>
-            <Allotment.Pane>
-              <div className="flex flex-col h-full gap-1">
-                <div className="flex-1"></div>
-                <div>
-                  <QuerySets />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <QueryAreaSelector />
-                  <div className="flex gap-2">
-                    <OSMQuerySubmit />
-                  </div>
-                </div>
-              </div>
-            </Allotment.Pane>
-          </Allotment>
-        </Allotment.Pane>
-
-        <Allotment.Pane className="pl-1">
+      <Allotment>
+        <Allotment.Pane>
           <Allotment vertical>
             <Allotment.Pane className={clsx(showStreetView && "pb-1")}>
               {view === "map" && <DynamicMap />}
