491e904e274479d1b04e4f456242686747bf1c8c
tlmn <mail@tlmn.io>
2023-08-21 16:09:01 +0200

    init

---
+++ src/components/dialogs/FilterDialog/relations/ContainsRelation.tsx
@@ -0,0 +1,61 @@
+import { TrashIcon } from "@radix-ui/react-icons";
+import React from "react";
+
+import Select from "@/components/Select";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import useImrStore from "@/stores/useImrStore";
+import { ContainsRelation } from "@/types/imr";
+
+const ContainsRelation = ({
+  edge: { id, src, tgt },
+}: {
+  edge: ContainsRelation;
+}) => {
+  const sets = useImrStore((state) => state.imr.ns);
+  const removeRelation = useImrStore((state) => state.removeRelation);
+  const setRelationValue = useImrStore((state) => state.setRelationValue);
+
+  return (
+    <div className="flex flex-col gap-1">
+      <div className="flex items-center justify-between gap-1">
+        <div className="flex items-center gap-2">
+          <span className="font-bold">Relation {id}</span>
+          <Badge>Contains</Badge>
+          <Button
+            variant="ghost"
+            className="p-1 aspect-square"
+            onClick={() => removeRelation(id)}
+          >
+            <TrashIcon scale={3} />
+          </Button>
+        </div>
+      </div>
+      <div className="flex justify-between flex-1 w-full">
+        <div className="flex items-center gap-2">
+          <Select
+            options={sets.map((set) =>
+              set.t === "nwr"
+                ? { label: set.n, value: set.id.toString() }
+                : { label: "Cluster", value: set.id.toString() }
+            )}
+            value={src.toString()}
+            onSelect={(value) => setRelationValue(id, "src", parseInt(value))}
+          />
+          contains
+          <Select
+            options={sets.map((set) =>
+              set.t === "nwr"
+                ? { label: set.n, value: set.id.toString() }
+                : { label: "Cluster", value: set.id.toString() }
+            )}
+            value={tgt.toString()}
+            onSelect={(value) => setRelationValue(id, "tgt", parseInt(value))}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContainsRelation;
---
+++ src/components/dialogs/FilterDialog/relations/DistanceRelation.tsx
@@ -0,0 +1,68 @@
+import { TrashIcon } from "@radix-ui/react-icons";
+import React from "react";
+
+import Select from "@/components/Select";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import useImrStore from "@/stores/useImrStore";
+import { DistanceRelation } from "@/types/imr";
+
+const DistanceRelation = ({
+  edge: { id, src, tgt, dist },
+}: {
+  edge: DistanceRelation;
+}) => {
+  const sets = useImrStore((state) => state.imr.ns);
+  const removeRelation = useImrStore((state) => state.removeRelation);
+  const setRelationValue = useImrStore((state) => state.setRelationValue);
+
+  return (
+    <div className="flex flex-col gap-1">
+      <div className="flex items-center justify-between gap-1">
+        <div className="flex items-center gap-2">
+          <span className="font-bold">Relation {id}</span>
+          <Badge className="h-6">Distance</Badge>
+          <Button
+            variant="ghost"
+            className="h-8 p-1 aspect-square"
+            onClick={() => removeRelation(id)}
+          >
+            <TrashIcon scale={3} />
+          </Button>
+        </div>
+      </div>
+      <div className="flex justify-between flex-1 w-full">
+        <div className="flex items-center gap-2">
+          <Select
+            options={sets.map((set) =>
+              set.t === "nwr"
+                ? { label: set.n, value: set.id.toString() }
+                : { label: "Cluster", value: set.id.toString() }
+            )}
+            value={src.toString()}
+            onSelect={(value) => setRelationValue(id, "src", parseInt(value))}
+          />
+          <Input
+            value={dist}
+            onChange={({ target: { value } }) =>
+              setRelationValue(id, "dist", value)
+            }
+          />
+          <span className="whitespace-nowrap">away from</span>
+          <Select
+            options={sets.map((set) =>
+              set.t === "nwr"
+                ? { label: set.n, value: set.id.toString() }
+                : { label: "Cluster", value: set.id.toString() }
+            )}
+            value={tgt.toString()}
+            onSelect={(value) => setRelationValue(id, "tgt", parseInt(value))}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DistanceRelation;
