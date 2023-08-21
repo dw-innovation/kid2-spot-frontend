8cfd43f49519eaf533782c6cc7fec04a0ab4ef9c
tlmn <mail@tlmn.io>
2023-08-21 12:38:37 +0200

    style

--- src/components/dialogs/FilterDialog/sets/Set.tsx
+++ src/components/dialogs/FilterDialog/sets/Set.tsx
@@ -1,27 +1,44 @@
-import { PlusIcon } from "@radix-ui/react-icons";
+import { PlusIcon, TrashIcon } from "@radix-ui/react-icons";
 import React from "react";
 
 import { Button } from "@/components/ui/button";
+import useImrStore from "@/stores/useImrStore";
 import { Filter, Node } from "@/types/imr";
 
 import NWRFilter from "./NWRFilter";
 
 const Set = ({ node }: { node: Node }) => {
+  const addFilter = useImrStore((state) => state.addFilter);
+  const removeFilter = useImrStore((state) => state.removeFilter);
   return (
     <>
       <span className="font-bold">Set {node.id}</span>{" "}
       <div className="flex flex-col gap-2 ml-8 font-normal">
         {node.t === "nwr" && (
           <>
-            <ol className="flex items-start list-decimal">
+            <ol className="flex flex-col items-start gap-1 list-decimal">
               {node.flts.map((filter: Filter, index: number) => (
-                <li key={index} className="flex items-start justify-start">
+                <li
+                  key={index}
+                  className="flex items-start justify-between w-full"
+                >
                   <NWRFilter filter={filter} filterId={index} setId={node.id} />
+                  <Button
+                    variant={"ghost"}
+                    className="p-1 aspect-square"
+                    onClick={() => removeFilter(node.id, index)}
+                  >
+                    <TrashIcon scale={3} />
+                  </Button>
                 </li>
               ))}
             </ol>
             <span>
-              <Button variant={"outline"} className="p-1">
+              <Button
+                variant={"outline"}
+                className="p-1"
+                onClick={() => addFilter(node.id)}
+              >
                 <PlusIcon /> add filter
               </Button>
             </span>
