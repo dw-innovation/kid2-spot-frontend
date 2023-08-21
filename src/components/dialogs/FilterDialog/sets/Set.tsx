17ad2bd66405212f891e95ae68ee824768f54efd
tlmn <mail@tlmn.io>
2023-08-21 16:09:16 +0200

    add removeNode button

--- src/components/dialogs/FilterDialog/sets/Set.tsx
+++ src/components/dialogs/FilterDialog/sets/Set.tsx
@@ -12,27 +12,37 @@ const Set = ({ node: { id, flts, n, t } }: { node: NWR | Cluster }) => {
   const addFilter = useImrStore((state) => state.addFilter);
   const removeFilter = useImrStore((state) => state.removeFilter);
   const setSetName = useImrStore((state) => state.setSetName);
+  const removeNode = useImrStore((state) => state.removeNode);
 
   useEffect(() => {
-    setSetName(id, `${flts[0].k}_${flts[0].v}`);
+    if (flts.length > 0) setSetName(id, `${flts[0].k}_${flts[0].v}`);
   }, [flts, id, setSetName]);
 
   return (
     <div className="flex flex-col gap-1">
       <div className="flex items-center justify-between gap-1">
-        <div className="flex gap-2">
+        <div className="flex items-center gap-2">
           <span className="font-bold">
             Set {id}: {n}
           </span>
-          <Badge>{t === "nwr" ? "NWR" : "Cluster"}</Badge>
+          <Badge className="h-6">{t === "nwr" ? "NWR" : "Cluster"}</Badge>
+          <Button
+            onClick={() => removeNode(id)}
+            variant="ghost"
+            className="h-8 p-1"
+          >
+            <TrashIcon scale={3} />
+          </Button>
         </div>
-        <Button
-          variant="outline"
-          className="h-8 p-1"
-          onClick={() => addFilter(id)}
-        >
-          <PlusIcon /> add filter
-        </Button>
+        <div className="flex gap-2 ">
+          <Button
+            variant="outline"
+            className="h-8 p-1"
+            onClick={() => addFilter(id)}
+          >
+            <PlusIcon /> add filter
+          </Button>
+        </div>
       </div>
       <section className="flex flex-col gap-1 ml-8 font-normal">
         {t === "nwr" && (
