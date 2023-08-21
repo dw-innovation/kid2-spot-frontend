b624b0f3244378240276a345bee82f9ee852b603
tlmn <mail@tlmn.io>
2023-08-21 16:26:46 +0200

    add cluster filter

--- src/components/dialogs/FilterDialog/sets/ClusterFilter.tsx
+++ src/components/dialogs/FilterDialog/sets/ClusterFilter.tsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const ClusterFilter = () => {
-  return <div>ClusterFilter</div>;
+import Select from "@/components/Select";
+import { Input } from "@/components/ui/input";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import useImrStore from "@/stores/useImrStore";
+import { Filter } from "@/types/imr";
+
+type Props = {
+  filter: Filter;
+  setId: number;
+  filterId: number;
+};
+
+const OPERATORS = [
+  { value: "=", label: "equals" },
+  { value: ">", label: "greater than" },
+  { value: "~", label: "contains" },
+];
+
+const ClusterFilter = ({ filter, setId, filterId }: Props) => {
+  const setFilterValue = useImrStore((state) => state.setFilterValue);
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    key: string
+  ) => {
+    setFilterValue(setId, filterId, key, e.target.value);
+  };
+
+  useEffect(() => {
+    setFilterValue(setId, filterId, "n", `${filter.k}_${filter.v}`);
+  }, [filter, filterId, setFilterValue, setId]);
+
+  return (
+    <div className="flex gap-1">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Input
+              value={filter.k}
+              className="flex-1 h-8"
+              onChange={(e) => handleInputChange(e, "k")}
+            />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>OSM Key</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger>
+            <Select
+              options={OPERATORS}
+              onSelect={(e) => setFilterValue(setId, filterId, "op", e)}
+              value={filter.op}
+              className="h-8"
+            />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Operator</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger>
+            <Input
+              value={filter.v}
+              className="flex-1 h-8"
+              onChange={(e) => handleInputChange(e, "v")}
+            />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>OSM value</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
 };
 
 export default ClusterFilter;
