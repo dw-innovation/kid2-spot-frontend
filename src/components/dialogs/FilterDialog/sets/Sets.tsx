0e30979fa2bc12b0fea474863d16052ec3570195
tlmn <mail@tlmn.io>
2023-08-21 16:09:28 +0200

    add add node buttons

--- src/components/dialogs/FilterDialog/sets/Sets.tsx
+++ src/components/dialogs/FilterDialog/sets/Sets.tsx
@@ -1,5 +1,7 @@
+import { PlusIcon } from "@radix-ui/react-icons";
 import React, { Fragment } from "react";
 
+import { Button } from "@/components/ui/button";
 import useImrStore from "@/stores/useImrStore";
 import { Node } from "@/types/imr";
 
@@ -7,9 +9,23 @@ import Set from "./Set";
 
 const Sets = () => {
   const imr = useImrStore((state) => state.imr);
+  const addNWRNode = useImrStore((state) => state.addNWRNode);
 
   return (
-    <div className="flex flex-col gap-4">
+    <fieldset className="flex flex-col gap-4 border-[1px] p-2">
+      <legend className="flex items-center gap-2 px-2 font-bold">
+        Sets
+        <Button
+          variant={"outline"}
+          className="h-8 p-1"
+          onClick={() => addNWRNode()}
+        >
+          <PlusIcon /> add NWR set
+        </Button>
+        <Button variant={"outline"} className="h-8 p-1" disabled>
+          <PlusIcon /> add cluster set
+        </Button>
+      </legend>
       {imr &&
         imr.ns &&
         imr.ns.map((node: Node) => (
@@ -17,7 +33,7 @@ const Sets = () => {
             {(node.t === "nwr" || node.t === "cluster") && <Set node={node} />}
           </Fragment>
         ))}
-    </div>
+    </fieldset>
   );
 };
 
