638c8031f170cf059ff85ca14d3807e0ca96c9f9
tlmn <mail@tlmn.io>
2023-08-21 16:08:46 +0200

    remove

--- src/components/dialogs/FilterDialog/relations/Relation.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { Edge } from "@/types/imr";
-
-const Set = ({}: { edge: Edge }) => {
-  return <div className="flex flex-col gap-1"></div>;
-};
-
-export default Set;
