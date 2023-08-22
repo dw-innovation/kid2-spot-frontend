102b5fc5b546694cf2c0e5d1da12a3eeef153163
tlmn <mail@tlmn.io>
2023-08-22 16:23:47 +0200

    reroute

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SymbolIcon } from "@radix-ui/react-icons";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 import FilterDialog from "@/components/dialogs/FilterDialog";
@@ -9,17 +10,16 @@ import ActionsMenu from "@/components/menus/ActionsMenu";
 import SettingsMenu from "@/components/menus/SettingsMenu";
 import OSMQuerySubmit from "@/components/OSMQuerySubmit";
 import { Button } from "@/components/ui/button";
-import useAppStore from "@/stores/useAppStore";
 
 const Header = () => {
-  const toggleStartScreen = useAppStore((state) => state.toggleStartScreen);
+  const router = useRouter();
 
   return (
     <div className="flex gap-2">
       <div className="flex items-center justify-between flex-1">
         <h1 className="pb-1 text-2xl font-bold leading-none">KID2 Spot</h1>
         <div className="flex gap-2">
-          <Button onClick={() => toggleStartScreen()}>
+          <Button onClick={() => router.push("/")}>
             <SymbolIcon />
             <span className="hidden md:block">New Search</span>
           </Button>
