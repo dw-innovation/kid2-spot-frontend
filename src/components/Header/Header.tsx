df024ecd444d1cdf24f6065bc16ca5c25dafd6f8
tlmn <mail@tlmn.io>
2023-08-22 17:50:45 +0200

    fix new search

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,16 +10,23 @@ import ActionsMenu from "@/components/menus/ActionsMenu";
 import SettingsMenu from "@/components/menus/SettingsMenu";
 import OSMQuerySubmit from "@/components/OSMQuerySubmit";
 import { Button } from "@/components/ui/button";
+import useAppStore from "@/stores/useAppStore";
 
 const Header = () => {
   const router = useRouter();
+  const resetSteps = useAppStore((state) => state.resetSteps);
 
   return (
     <div className="flex gap-2">
       <div className="flex items-center justify-between flex-1">
         <h1 className="pb-1 text-2xl font-bold leading-none">KID2 Spot</h1>
         <div className="flex gap-2">
-          <Button onClick={() => router.push("/")}>
+          <Button
+            onClick={() => {
+              resetSteps();
+              router.push("/");
+            }}
+          >
             <SymbolIcon />
             <span className="hidden md:block">New Search</span>
           </Button>
