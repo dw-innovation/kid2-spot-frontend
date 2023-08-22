fff19f448862507604fffbdd911ed41c34510872
tlmn <mail@tlmn.io>
2023-08-22 16:23:52 +0200

    add size prop

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,6 +1,14 @@
 import { Loader2 } from "lucide-react";
 import React from "react";
 
-const LoadingSpinner = () => <Loader2 className="w-4 h-4 animate-spin" />;
+const LoadingSpinner = ({ size = "1rem" }: { size?: string }) => (
+  <Loader2
+    className="w-4 h-4 animate-spin"
+    style={{
+      width: size,
+      height: size,
+    }}
+  />
+);
 
 export default LoadingSpinner;
