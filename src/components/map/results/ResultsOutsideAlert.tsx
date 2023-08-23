9f78e9c91a75276a3ee8d36ce9a7ab0b9e5ca338
tlmn <mail@tlmn.io>
2023-08-23 10:06:19 +0200

    set max w and close on click

--- src/components/map/results/ResultsOutsideAlert.tsx
+++ src/components/map/results/ResultsOutsideAlert.tsx
@@ -21,6 +21,7 @@ const ResultsOutsideAlert = () => {
   const [showAlert, setShowAlert] = useState(false);
 
   const handleFlyToBounds = () => {
+    handleCloseClick();
     let newBBox = bbox(geoJSON);
 
     setBounds([
@@ -78,7 +79,7 @@ const ResultsOutsideAlert = () => {
   return (
     <>
       {showAlert && (
-        <span className="relative flex items-center justify-center gap-2 px-2 py-1 mr-2 text-black bg-orange-400 rounded-lg shadow-lg">
+        <span className="max-w-[15rem] relative flex items-center justify-center gap-2 px-2 py-1 mr-2 text-black bg-orange-400 rounded-lg shadow-lg">
           Results outside bounding box!
           <button
             onClick={handleFlyToBounds}
