9da88f965cf424c0a331b7d37c9ee3e21c861e86
tlmn <mail@tlmn.io>
2023-09-04 10:09:57 +0200

    style overflow

--- src/components/addressSearch/AddressSuggestions.tsx
+++ src/components/addressSearch/AddressSuggestions.tsx
@@ -31,33 +31,31 @@ const AddressSuggestions = ({
   selectedItem,
   getItemProps,
   getMenuProps,
-}: AddressSuggestionProps) => {
-  return (
-    <ul
-      className={`absolute w-full bg-white mt-1 shadow-md max-h-80 p-0 z-[800] ${
-        !isOpen && suggestions.length === 0 && "hidden"
-      }`}
-      {...getMenuProps()}
-    >
-      {isOpen &&
-        suggestions.map((item, index) => (
-          <li
-            key={index}
-            className={clsx(
-              highlightedIndex === index && "bg-blue-300",
-              selectedItem === item && "font-bold",
-              "py-2 px-3 shadow-sm flex flex-col cursor-pointer"
-            )}
-            {...getItemProps({
-              index,
-              item,
-            })}
-          >
-            {item.place_name}
-          </li>
-        ))}
-    </ul>
-  );
-};
+}: AddressSuggestionProps) => (
+  <ul
+    className={`absolute w-full bg-white mt-1 shadow-md max-h-80 p-0 z-[800] overflow-y-auto ${
+      !isOpen && suggestions.length === 0 && "hidden"
+    }`}
+    {...getMenuProps()}
+  >
+    {isOpen &&
+      suggestions.map((item, index) => (
+        <li
+          key={index}
+          className={clsx(
+            highlightedIndex === index && "bg-blue-300",
+            selectedItem === item && "font-bold",
+            "py-2 px-3 shadow-sm flex flex-col cursor-pointer"
+          )}
+          {...getItemProps({
+            index,
+            item,
+          })}
+        >
+          {item.place_name}
+        </li>
+      ))}
+  </ul>
+);
 
 export default AddressSuggestions;
