517e2d584e624f5a19ec4031f786238faf336440
tlmn <mail@tlmn.io>
2023-09-07 15:13:33 +0200

    add sessions store

---
+++ src/stores/interfaces/SessionsStore.interface.ts
@@ -0,0 +1,30 @@
+import { UUID } from "short-uuid";
+
+import AppStoreInterface from "./AppStore.interface";
+import ImrStoreInterface from "./ImrStore.interface";
+import MapStoreInterface from "./MapStore.interface";
+import QueryStoreInterface from "./QueryStore.interface";
+import StreetViewStoreInterface from "./StreetViewStore.interface";
+
+export type Session = {
+  id: UUID;
+  name: string;
+  description: string;
+  created: string;
+  modified: string;
+  data: {
+    useAppStore: AppStoreInterface;
+    useMapStore: MapStoreInterface;
+    useQueryStore: QueryStoreInterface;
+    useStreetViewStore: StreetViewStoreInterface;
+    useImrStore: ImrStoreInterface;
+  };
+};
+
+export default interface SessionsStoreInterface {
+  sessions: Session[];
+  addSession: (session: Session) => void;
+  removeSession: (id: string) => void;
+  updateSession: (session: Session) => void;
+  clearSessions: () => void;
+}
---
+++ src/stores/useSessionsStore.ts
@@ -0,0 +1,53 @@
+import produce from "immer";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+import SessionsStore, { Session } from "./interfaces/SessionsStore.interface";
+
+const useSessionsStore = create<SessionsStore>()(
+  persist(
+    (set) => ({
+      sessions: [],
+      addSession: (session: Session) => {
+        set(
+          produce((draft) => {
+            draft.sessions.push(session);
+          })
+        );
+      },
+      removeSession: (id: string) => {
+        set(
+          produce((draft) => {
+            draft.sessions = draft.sessions.filter(
+              (session: Session) => session.id !== id
+            );
+          })
+        );
+      },
+      updateSession: (session: Session) => {
+        set(
+          produce((draft) => {
+            const index = draft.sessions.findIndex(
+              (s: Session) => s.id === session.id
+            );
+            if (index !== -1) {
+              draft.sessions[index] = session;
+            }
+          })
+        );
+      },
+      clearSessions: () => {
+        set(
+          produce((draft) => {
+            draft.sessions = [];
+          })
+        );
+      },
+    }),
+    {
+      name: "spot-sessions",
+    }
+  )
+);
+
+export default useSessionsStore;
