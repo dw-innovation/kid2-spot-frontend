cd197b22b57a51fe25d80a039c19daeb29f5aa47
tlmn <mail@tlmn.io>
2023-09-12 16:12:23 +0200

    change route

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -125,7 +125,7 @@ export const translateNLtoIMR = async (
 ): Promise<any> => {
   const response = await axios({
     method: "POST",
-    url: `${process.env.NEXT_PUBLIC_NLP_API}/transform-sentence-to-imr`,
+    url: `${process.env.NEXT_PUBLIC_NLP_API}/translate_from_nl_to_op`,
     data: {
       sentence: naturalLanguagePrompt,
     },
