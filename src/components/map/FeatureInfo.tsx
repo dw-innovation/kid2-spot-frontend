b172e7b8d46efff7545d90bdd7e1313b85c6307f
tlmn <mail@tlmn.io>
2023-09-14 06:26:44 +0200

    render osm info

--- src/components/map/FeatureInfo.tsx
+++ src/components/map/FeatureInfo.tsx
@@ -6,26 +6,25 @@ type Props = {
 
 const FeatureInfo = ({ feature }: Props) => (
   <div>
-    <div className="pb-2 w-36">
+    <div className="pb-2">
       {feature.properties.tags.name && (
         <h3 className="text-lg font-bold leading-tight">
           {feature.properties.name}
         </h3>
       )}
-      {feature.properties.osm_id && (
-        <a
-          href={`https://www.openstreetmap.org/node/${feature.properties.osm_id}`}
-          target="_blank"
-          rel="noreferrer"
-          className="block"
-        >
-          {feature.properties.osm_id}
-        </a>
-      )}
+      <a
+        href={`https://www.openstreetmap.org/${feature.properties.osm_ids}`}
+        target="_blank"
+        rel="noreferrer"
+        className="block"
+      >
+        Show Info on OpenStreetMap
+      </a>
     </div>
     <span className="text-lg font-bold uppercase">
       Tags ({Object.keys(feature.properties.tags).length})
     </span>
+
     {Object.keys(feature.properties.tags).map((key, index) => (
       <span key={index} className="block">
         <strong>{key}</strong>: {feature.properties.tags[key]}
