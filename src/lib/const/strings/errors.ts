c50847a75d2f6ec09301ddf0e36f0d8fbe82eef5
tlmn <mail@tlmn.io>
2023-09-14 06:27:15 +0200

    add error message

--- src/lib/const/strings/errors.ts
+++ src/lib/const/strings/errors.ts
@@ -2,5 +2,7 @@ export const STRINGS = {
   noResults: "No results found in the selected area.",
   areaInvalid: "The selected area is invalid.",
   areaTooLarge: "The selected area is too large.",
+  imrInvalid:
+    "There is an error in the structure of the intermediate representation (IMR).",
   timeout: "The request timed out.",
 };
