3a30f824132c0740b0991ddef25f1c36a8acdef0
tlmn <mail@tlmn.io>
2023-09-16 21:43:44 +0200

    position center

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
 
   return (
     <div className="flex gap-2">
-      <div className="flex items-start justify-between flex-1">
+      <div className="flex items-center justify-between flex-1">
         <h1 className="pb-1 text-2xl font-bold leading-none">Spot</h1>
         <div className="items-start hidden gap-2 md:flex">
           <OSMQuerySubmit />
