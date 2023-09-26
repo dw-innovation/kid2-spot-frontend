b7ac7959b774310fd579027b840d7335412b083d
tlmn <mail@tlmn.io>
2023-09-26 17:45:11 +0200

    revamp prompt input

--- src/components/map/SettingsPanel/Prompt.tsx
+++ src/components/map/SettingsPanel/Prompt.tsx
@@ -3,16 +3,30 @@ import React from "react";
 
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import useGlobalStore from "@/stores/useGlobalStore";
 import useImrStore from "@/stores/useImrStore";
 
 const Prompt = () => {
   const nlSentence = useImrStore((state) => state.nlSentence);
+  const setNlSentence = useImrStore((state) => state.setNlSentence);
+  const resetSteps = useGlobalStore((state) => state.resetSteps);
+  const nextStep = useGlobalStore((state) => state.nextStep);
+
+  const toggleDialog = useGlobalStore((state) => state.toggleDialog);
+
+  const handleSearchClick = () => {
+    resetSteps();
+    nextStep();
+    toggleDialog("inputStepper");
+  };
 
   return (
     <div className="flex flex-col gap-2">
-      <h3 className="text-lg font-semibold ">Initial Query</h3>
-      <Textarea>{nlSentence}</Textarea>
-      <Button>
+      <h3 className="text-lg font-semibold">Your search</h3>
+      <Textarea onChange={({ target: { value } }) => setNlSentence(value)}>
+        {nlSentence}
+      </Textarea>
+      <Button onClick={handleSearchClick}>
         <SearchIcon className="w-4 h-4" />
         Search
       </Button>
