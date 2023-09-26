5c9ba28e97474089bbfc751b62c99f2bedc0868b
tlmn <mail@tlmn.io>
2023-09-26 17:45:25 +0200

    remove default sentence

--- src/stores/useImrStore.ts
+++ src/stores/useImrStore.ts
@@ -13,7 +13,7 @@ import {
 import ImrStoreInterface from "../types/stores/ImrStore.interface";
 
 const useImrStore = create<ImrStoreInterface>((set) => ({
-  nlSentence: "Find all hotels no more than 200m from a train station in Köln.",
+  nlSentence: "",
   setNlSentence: (nlSentence: string) => {
     set(
       produce((draft) => {
