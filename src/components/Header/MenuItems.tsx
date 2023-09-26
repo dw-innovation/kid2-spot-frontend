709132b6832c678a67f3f133c9d0b1dbfab5e734
tlmn <mail@tlmn.io>
2023-09-26 17:14:50 +0200

    remove legacy imports

--- src/components/Header/MenuItems.tsx
+++ src/components/Header/MenuItems.tsx
@@ -1,23 +1,14 @@
 "use client";
 
-import { Pencil1Icon, SymbolIcon } from "@radix-ui/react-icons";
-import { useRouter } from "next/navigation";
+import { Pencil1Icon } from "@radix-ui/react-icons";
 import React from "react";
 
 import ActionsMenu from "@/components/menus/ActionsMenu";
 import SettingsMenu from "@/components/menus/SettingsMenu";
 import { Button } from "@/components/ui/button";
-import { useStrings } from "@/lib/contexts/useStrings";
 import useGlobalStore from "@/stores/useGlobalStore";
 
-import { useMenu } from "./Context";
-
 const MenuItems = () => {
-  const resetSteps = useGlobalStore((state) => state.resetSteps);
-  const router = useRouter();
-  const { setOpen } = useMenu();
-  const { commonNewSearchButton } = useStrings();
-
   const toggleDialog = useGlobalStore((state) => state.toggleDialog);
 
   return (
