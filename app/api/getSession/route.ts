2082925ba0b9c074d2523247c3f6abb2218700a2
tlmn <mail@tlmn.io>
2023-10-12 08:50:53 +0200

    comment

--- app/api/getSession/route.ts
+++ app/api/getSession/route.ts
@@ -3,11 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 let cachedDb: Db;
 
+// Function to connect to MongoDB, reusing a cached connection if available
 async function connectToDatabase(): Promise<Db> {
   if (cachedDb) return cachedDb;
 
   const client = await MongoClient.connect(process.env.MONGODB_URI || "");
-
   const db = client.db(process.env.MONGODB_DBNAME);
   cachedDb = db;
   return db;
@@ -16,6 +16,8 @@ async function connectToDatabase(): Promise<Db> {
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const id = searchParams.get("id");
+
+  // Validate if 'id' query parameter exists
   if (!id) {
     return NextResponse.json(
       { message: "missingSessionId", status: "error" },
@@ -24,11 +26,14 @@ export async function GET(req: NextRequest) {
   }
 
   try {
+    // Connect to MongoDB and get the 'sessions' collection
     const db = await connectToDatabase();
     const collection = db.collection("sessions");
 
+    // Search for the session by its 'id'
     const result = await collection.findOne({ id: id });
 
+    // Send back the found session data or null if not found
     return NextResponse.json(
       {
         status: "success",
@@ -38,6 +43,7 @@ export async function GET(req: NextRequest) {
       { status: 200 }
     );
   } catch (error: any) {
+    // Handle any errors during the database operation
     return NextResponse.json(
       {
         status: "error",
--- app/api/saveSession/route.ts
+++ app/api/saveSession/route.ts
@@ -4,19 +4,24 @@ import shortUUID from "short-uuid";
 
 let cachedDb: Db;
 
+// Function to connect to MongoDB, reusing a cached connection if available
 async function connectToDatabase(): Promise<Db> {
   if (cachedDb) return cachedDb;
 
   const client = await MongoClient.connect(process.env.MONGODB_URI || "");
-
   const db = client.db(process.env.MONGODB_DBNAME);
   cachedDb = db;
   return db;
 }
 
 export async function POST(req: NextRequest) {
+  // Generate a short UUID for the new session
   const id = shortUUID.generate();
+
+  // Parse the request payload
   const payload = await req.json();
+
+  // Construct the data object to be inserted into the database
   const data = {
     id: id,
     settings: payload,
@@ -24,20 +29,24 @@ export async function POST(req: NextRequest) {
   };
 
   try {
+    // Connect to MongoDB and get the 'sessions' collection
     const db = await connectToDatabase();
     const collection = db.collection("sessions");
 
+    // Insert the new session data into the database
     await collection.insertOne(data);
 
+    // Return a success response along with the generated ID
     return NextResponse.json(
       {
         status: "success",
-        message: "dataAddedSuccessfully",
+        message: "sessionSuccessfullySaved",
         id: id,
       },
       { status: 200 }
     );
   } catch (error: any) {
+    // Handle any errors during the database operation
     return NextResponse.json(
       {
         status: "error",
