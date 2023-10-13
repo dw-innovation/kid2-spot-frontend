c68d435cd055ccf5c3a07cdcb185a9f2871161ef
tlmn <mail@tlmn.io>
2023-10-13 07:19:37 +0200

    render current search area setting

--- src/components/QueryPanel/SearchArea.tsx
+++ src/components/QueryPanel/SearchArea.tsx
@@ -1,32 +1,17 @@
 import React from "react";
 
-import { Button } from "@/components/ui/button";
-import useStrings from "@/lib/contexts/useStrings";
 import useImrStore from "@/stores/useImrStore";
 
 const SearchArea = () => {
+  const currentSearchArea = useImrStore((state) => state.imr.area.value);
   const searchAreaType = useImrStore((state) => state.imr.area.type);
-
-  const { settingsMenuCustomArea, settingsMenuSearchBbox } = useStrings();
-
-  const SEARCH_AREAS = [
-    { label: settingsMenuSearchBbox(), value: "bbox" },
-    { label: settingsMenuCustomArea(), value: "area" },
-  ];
 
   return (
     <div>
       <h3 className="text-lg font-semibold ">Location</h3>
-      <div className="flex gap-2">
-        {SEARCH_AREAS.map(({ label, value }) => (
-          <Button
-            key={label}
-            variant={searchAreaType === value ? "secondary" : "outline"}
-            size={"sm"}
-          >
-            {label}
-          </Button>
-        ))}
+      <div className="flex gap-2 ml-4">
+        {searchAreaType === "area" && currentSearchArea}
+        {searchAreaType === "bbox" && "Current Map View"}
       </div>
     </div>
   );
