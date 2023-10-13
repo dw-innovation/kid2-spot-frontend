1c2ddaf66a3c65f73735e8dedf140a69b0bca20d
tlmn <mail@tlmn.io>
2023-10-13 13:59:57 +0200

    memoize position

--- src/components/StreetViewPane/StreetViewPane.tsx
+++ src/components/StreetViewPane/StreetViewPane.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import clsx from "clsx";
-import React, { useEffect } from "react";
-import Draggable from "react-draggable"; // Import the react-draggable library
+import React, { useEffect, useState } from "react";
+import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 import { Resizable } from "react-resizable";
 
 import FullScreenCloseIcon from "@/assets/icons/FullScreenCloseIcon";
@@ -14,6 +14,11 @@ import useGlobalStore from "@/stores/useGlobalStore";
 import useStreetViewStore from "@/stores/useStreetViewStore";
 
 const StreetViewPane = () => {
+  const [dragData, setDragData] = useState({ x: 0, y: 0 });
+  const [memoizedDragData, setMemoizedDragData] = useState<{
+    x: number;
+    y: number;
+  } | null>(null);
   const isStreetViewFullscreen = useGlobalStore(
     (state) => state.isStreetViewFullscreen
   );
@@ -29,10 +34,23 @@ const StreetViewPane = () => {
   const showStreetView = useStreetViewStore((state) => state.showStreetView);
 
   const handleFullscreenClick = () => {
+    if (isStreetViewFullscreen) {
+      if (memoizedDragData) {
+        setDragData(memoizedDragData);
+      }
+    } else {
+      setMemoizedDragData(dragData);
+      setDragData({ x: 0, y: 0 });
+    }
     toggleStreetViewFullscreen();
   };
+
   const handleCloseClick = () => {
     toggleStreetView(false);
+  };
+
+  const handleDrag = (_: DraggableEvent, data: DraggableData) => {
+    setDragData({ x: data.x, y: data.y });
   };
 
   useEffect(() => {
@@ -40,7 +58,7 @@ const StreetViewPane = () => {
   }, [toggleStreetViewFullscreen]);
 
   return (
-    <Draggable handle=".drag-handle">
+    <Draggable handle=".drag-handle" position={dragData} onDrag={handleDrag}>
       <Resizable width={600} height={300}>
         <div
           className={clsx(
