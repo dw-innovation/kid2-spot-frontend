e51bf758ffaf9aa6d9807eda7cb5fba662cf68f9
tlmn <mail@tlmn.io>
2023-10-13 15:10:01 +0200

    move controls to header

--- src/components/StreetViewPane/StreetViewPane.tsx
+++ src/components/StreetViewPane/StreetViewPane.tsx
@@ -1,14 +1,11 @@
 "use client";
 
+import { Cross1Icon, MoveIcon, SizeIcon } from "@radix-ui/react-icons";
 import clsx from "clsx";
 import React, { useEffect, useState } from "react";
 import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 import { Resizable } from "react-resizable";
 
-import FullScreenCloseIcon from "@/assets/icons/FullScreenCloseIcon";
-import FullScreenExpandIcon from "@/assets/icons/FullScreenExpandIcon";
-import PlusIcon from "@/assets/icons/PlusIcon";
-import { Button } from "@/components/ui/button";
 import { cn, createGoogleMapsEmbedUrl } from "@/lib/utils";
 import useGlobalStore from "@/stores/useGlobalStore";
 import useStreetViewStore from "@/stores/useStreetViewStore";
@@ -46,6 +43,7 @@ const StreetViewPane = () => {
   };
 
   const handleCloseClick = () => {
+    toggleStreetViewFullscreen(false);
     toggleStreetView(false);
   };
 
@@ -70,27 +68,28 @@ const StreetViewPane = () => {
           )}
         >
           <div
-            className={cn("drag-handle", isStreetViewFullscreen && "hidden")}
+            className={cn(
+              "drag-handle",
+              isStreetViewFullscreen && "hidden",
+              "flex justify-between px-2 cursor-grab"
+            )}
           >
-            <span className="p-2 font-sans font-semibold">
-              Google Street View
-            </span>
+            <div className="flex items-center gap-1">
+              <MoveIcon scale={20} />
+              <span className="p-2 font-sans font-semibold">
+                Google Street View
+              </span>
+            </div>
+            <div className="flex gap-2">
+              <button onClick={handleFullscreenClick}>
+                <SizeIcon scale={20} />
+              </button>
+              <button onClick={handleCloseClick}>
+                <Cross1Icon scale={20} />
+              </button>
+            </div>
           </div>
           <div className="relative w-full h-full">
-            <div className="absolute top-0 right-0 z-20 flex items-end gap-2 m-2">
-              <Button onClick={handleFullscreenClick}>
-                {isStreetViewFullscreen ? (
-                  <FullScreenCloseIcon />
-                ) : (
-                  <FullScreenExpandIcon />
-                )}
-              </Button>
-              <Button onClick={handleCloseClick} className="aspect-square">
-                <div className="rotate-45">
-                  <PlusIcon />
-                </div>
-              </Button>
-            </div>
             <iframe
               src={createGoogleMapsEmbedUrl(streetViewCoordinates)}
               width="100%"
