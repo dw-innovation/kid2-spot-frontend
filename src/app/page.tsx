69c803da6ae4c44e189160138a9cfb080aa84d0d
tlmn <mail@tlmn.io>
2023-10-14 08:06:29 +0200

    start with map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,15 @@
-import { Metadata } from "next";
 import React from "react";
 
 import Dialogs from "@/components/dialogs";
 import Div100vh from "@/components/Div100vh";
-import InputStepper from "@/components/InputStepper";
+import Interface from "@/components/Interface";
 import { StringProvider } from "@/lib/contexts/useStrings";
-
-export const metadata: Metadata = {
-  title: "Spot – Search the world with your words",
-  viewport:
-    "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0",
-};
 
 const Page = () => (
   <StringProvider>
     <Div100vh>
-      <InputStepper />
       <Dialogs />
+      <Interface />
     </Div100vh>
   </StringProvider>
 );
