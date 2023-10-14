16e1994472491df7064465b7d363d4344d75f708
tlmn <mail@tlmn.io>
2023-10-14 08:09:55 +0200

    trigger filter dialog

--- src/components/QueryPanel/Entities/Entity.tsx
+++ src/components/QueryPanel/Entities/Entity.tsx
@@ -1,23 +1,32 @@
 import { Edit2Icon } from "lucide-react";
 import React from "react";
 
+import useGlobalStore from "@/stores/useGlobalStore";
+
 type Props = {
   name: string;
   id: number;
 };
 
-const Entity = ({ name, id }: Props) => {
-  const handleEditClick = (id: number) => {
-    console.log("edit", id);
+const Entity = ({ name }: Props) => {
+  const toggleDialog = useGlobalStore((state) => state.toggleDialog);
+
+  const handleEditClick = () => {
+    toggleDialog("entityFilters");
   };
 
   return (
-    <div className="flex gap-1 p-1 capitalize bg-red-200 rounded-md w-fit">
-      <span>{name}</span>
-      <span className="border-r-[1px] border-black my-[2px]"></span>
-      <button onClick={() => handleEditClick(id)}>
-        <Edit2Icon size={10} />
-      </button>
+    <div className="flex gap-1 capitalize rounded-md w-fit border-[1px] border-black overflow-clip">
+      <span className="p-1">{name}</span>
+      <div className="flex h-full">
+        <span className="border-r-[1px] border-black " />
+        <button
+          onClick={() => handleEditClick()}
+          className="p-1 hover:bg-primary hover:text-white aspect-square"
+        >
+          <Edit2Icon size={10} />
+        </button>
+      </div>
     </div>
   );
 };
