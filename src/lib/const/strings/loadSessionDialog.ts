193d349c5e2149698a3c6324b408cb10a54411f6
tlmn <mail@tlmn.io>
2023-10-25 15:51:17 +0200

    update texts

--- src/lib/const/strings/loadSessionDialog.ts
+++ src/lib/const/strings/loadSessionDialog.ts
@@ -1,6 +1,7 @@
 export const STRINGS = {
   title: "Load a Session",
-  description: "Load a saved session from your browser.",
+  description:
+    "Load a saved session from your browser.<br/>Note: This does not work across browsers",
   loadSessionButton: "Load Session",
   removeSessionButton: "Remove Session",
   sessionSavedInfo: "Session saved on {date} at {time}.",
--- src/lib/const/strings/saveSessionDialog.ts
+++ src/lib/const/strings/saveSessionDialog.ts
@@ -1,7 +1,7 @@
 export const STRINGS = {
   title: "Save Current Session",
   description:
-    "You can save your current session in your browser to restore it at a later point in time.",
+    "You can save your current search in your browser to restore it at a later point in time.<br/>Note: clearing your cookies or other site data will also clear the session data.",
   placeholderName: "Session name",
   placeholderDescription: "Add a description",
   saveButton: "Save Session Locally",
