2f1995bfffd51e154e112687e86ecac0f83f1958
tlmn <mail@tlmn.io>
2024-02-01 14:06:03 +0100

    use useMutation

--- src/components/menus/ActionsMenu/ActionsMenu.tsx
+++ src/components/menus/ActionsMenu/ActionsMenu.tsx
@@ -6,20 +6,13 @@ import {
   UploadIcon,
 } from "@radix-ui/react-icons";
 import React, { useState } from "react";
+import { useMutation } from "react-query";
+import { toast } from "react-toastify";
 
 import { useMenu } from "@/components/Header/Context";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import useStrings from "@/lib/contexts/useStrings";
-import useApiStatus from "@/lib/hooks/useApiStatus";
 import { saveSession } from "@/lib/sessions";
 import useGlobalStore from "@/stores/useGlobalStore";
 import useImrStore from "@/stores/useImrStore";
@@ -29,35 +22,62 @@ import useResultsStore from "@/stores/useResultsStore";
 import useSessionsStore from "@/stores/useSessionsStore";
 import useStreetViewStore from "@/stores/useStreetViewStore";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../../ui/dropdown-menu";
+
 const ActionsMenu = () => {
   const {
     actionMenuTrigger,
-    actionMenuDownloadResults,
+    actionMenuSessionTitle,
+    actionMenuShareSession,
+    actionMenuSaveSession,
     actionMenuLoadSession,
     actionMenuResultsTitle,
-    actionMenuSaveSession,
-    actionMenuShareSession,
-    actionMenuSessionTitle,
+    actionMenuDownloadResults,
   } = useStrings();
-  const geoJSON = useResultsStore((state) => state.geoJSON);
-  const isGeoJSONAvailable = Boolean(geoJSON);
+
   const [open, setOpen] = useState(false);
   const { setOpen: setMenuOpen } = useMenu();
+  const geoJSON = useResultsStore((state) => state.geoJSON);
+  const isGeoJSONAvailable = Boolean(geoJSON);
+
   const toggleDialog = useGlobalStore((state) => state.toggleDialog);
   const sessions = useSessionsStore((state) => state.sessions);
-
-  const [apiStatus, triggerSaveData] = useApiStatus(() =>
-    saveSession([
-      { name: "useGlobalStore", getState: useGlobalStore.getState },
-      { name: "useMapStore", getState: useMapStore.getState },
-      { name: "useQueryStore", getState: useQueryStore.getState },
-      { name: "useStreetViewStore", getState: useStreetViewStore.getState },
-      { name: "useImrStore", getState: useImrStore.getState },
-    ])
+  const mutation = useMutation(
+    () =>
+      saveSession([
+        { name: "useGlobalStore", getState: useGlobalStore.getState },
+        { name: "useMapStore", getState: useMapStore.getState },
+        { name: "useQueryStore", getState: useQueryStore.getState },
+        { name: "useStreetViewStore", getState: useStreetViewStore.getState },
+        { name: "useImrStore", getState: useImrStore.getState },
+      ]),
+    {
+      onSuccess: (sessionLink) => {
+        window.navigator.clipboard.writeText(sessionLink);
+        toast.success("Session saved and share link copied to clipboard!");
+      },
+      onError: (error) => {
+        if (error instanceof Error) {
+          toast.error("Error saving session: " + error.message);
+        } else {
+          toast.error("An unknown error occurred while saving the session");
+        }
+      },
+    }
   );
 
-  const ShareSessionIcon =
-    apiStatus === "loading" ? <LoadingSpinner /> : <Share1Icon />;
+  const ShareSessionIcon = mutation.isLoading ? (
+    <LoadingSpinner />
+  ) : (
+    <Share1Icon />
+  );
 
   return (
     <DropdownMenu
@@ -85,10 +105,7 @@ const ActionsMenu = () => {
         <DropdownMenuItem
           onClick={async (e) => {
             e.preventDefault();
-            triggerSaveData().then(() => {
-              setOpen(false);
-              setMenuOpen(false);
-            });
+            mutation.mutate();
           }}
         >
           {ShareSessionIcon}
