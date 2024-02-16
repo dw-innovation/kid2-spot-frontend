868134f3de5f7f31486b70cdcf19dbe1d9faa6f6
tlmn <mail@tlmn.io>
2024-02-16 16:28:36 +0100

    fix connectors

--- src/components/dialogs/EntityEditor/Connectors.tsx
+++ src/components/dialogs/EntityEditor/Connectors.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Connectors = () => (
-  <div className="flex flex-col self-stretch w-3 overflow-visible connector">
+  <div className="relative self-stretch float-left w-3 overflow-visible connector">
     <div className="up-connector" />
     <div className="down-connector" />
   </div>
