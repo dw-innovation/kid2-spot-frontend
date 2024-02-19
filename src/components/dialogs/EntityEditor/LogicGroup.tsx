9736e0a3acb0c981227d1292468e212f9c93c67e
tlmn <mail@tlmn.io>
2024-02-19 17:52:40 +0100

    fix: fix bug in removing array element from clonedImr object
    feat: add logging of filters object in Dialog component

--- src/components/dialogs/EntityEditor/LogicGroup.tsx
+++ src/components/dialogs/EntityEditor/LogicGroup.tsx
@@ -41,6 +41,7 @@ const LogicGroup = ({ filterNode, path, nodeId, pathString }: Props) => {
       )}
     >
       <Connectors />
+      {pathString}
       <div
         className={cn(
           "flex items-center p-2",
