33a2b3eb130c20ab817de30b34c8faabfb7a8d32
tlmn <mail@tlmn.io>
2024-02-20 20:36:51 +0100

    enable titles

--- src/components/dialogs/ErrorDialog/ErrorDialog.tsx
+++ src/components/dialogs/ErrorDialog/ErrorDialog.tsx
@@ -10,18 +10,27 @@ import Dialog from "../Dialog";
 const DIALOG_NAME = "error";
 
 const ErrorDialog = () => {
-  const { errorDialogTitle, errorDialogCloseButton } = useStrings();
+  const { errorDialogCloseButton } = useStrings();
   const toggleDialog = useGlobalStore((state) => state.toggleDialog);
   const errorType = useGlobalStore((state) => state.errorType);
   const clearError = useGlobalStore((state) => state.clearError);
 
+  const errorKey = Object.keys(STRINGS).find(
+    (key) => key === errorType && !key.endsWith("Title")
+  );
+
+  const dialogTitle = errorKey
+    ? STRINGS[errorKey as keyof typeof STRINGS]
+    : errorType;
+
+  const dialogDescription =
+    STRINGS[errorType as keyof typeof STRINGS] || errorType;
+
   return (
     <Dialog
       dialogName={DIALOG_NAME}
-      dialogTitle={errorDialogTitle()}
-      dialogDescription={
-        STRINGS[errorType as keyof typeof STRINGS] || errorType
-      }
+      dialogTitle={dialogTitle}
+      dialogDescription={dialogDescription}
     >
       <Button
         onClick={() => {
--- src/lib/const/strings/errors.ts
+++ src/lib/const/strings/errors.ts
@@ -1,7 +1,12 @@
 export const STRINGS = {
+  noResultsTitle: "No Results",
   noResults: "We couldn't find any results in the selected area.",
+  areaInvalidTitle: "Area Invalid",
   areaInvalid: "The selected area is invalid.",
+  areaTooLargeTitle: "Area Too Large",
   areaTooLarge: "The selected area is too large.",
+  imrInvalidTitle: "Invalid IMR",
   imrInvalid: "The was an error when analyzing your prompt.",
+  timeoutTitle: "Query Timeout",
   timeout: "Your request timed out.",
 };
