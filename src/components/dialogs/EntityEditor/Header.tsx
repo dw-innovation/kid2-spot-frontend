29b696e3d70ee50d4ea3d0752a2ce819d357c9e4
tlmn <mail@tlmn.io>
2024-02-22 17:07:41 +0100

    capitalize

--- src/components/dialogs/EntityEditor/Header.tsx
+++ src/components/dialogs/EntityEditor/Header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import { capitalize } from "@/lib/utils";
+
 type Props = {
   name: string;
 };
 
 const Header = ({ name }: Props) => (
   <h2 className="text-xl font-bold">
-    Edit OSM tags and rules for &quot;{name}&quot;
+    Edit OSM tags and rules for &quot;{capitalize(name)}&quot;
   </h2>
 );
 
--- src/components/dialogs/ErrorDialog/DetectedEntitiesBar.tsx
+++ src/components/dialogs/ErrorDialog/DetectedEntitiesBar.tsx
@@ -1,4 +1,5 @@
 import { Pencil1Icon } from "@radix-ui/react-icons";
+import { capitalize } from "lodash";
 import React from "react";
 
 import { Button } from "@/components/ui/button";
@@ -19,7 +20,7 @@ const DetectedEntitiesBar = () => {
     <div className="flex gap-2">
       {nodes.map(({ id, name }, index) => (
         <Button key={index} onClick={() => handleEntityClick(id)}>
-          <Pencil1Icon /> {name}
+          <Pencil1Icon /> {capitalize(name)}
         </Button>
       ))}
     </div>
