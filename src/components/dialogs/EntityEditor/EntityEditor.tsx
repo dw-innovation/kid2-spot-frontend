265a7a94be9a931dffc2a41b7c7207a23eca1069
tlmn <mail@tlmn.io>
2024-02-27 14:40:51 +0100

    catch empty filters

--- src/components/dialogs/EntityEditor/EntityEditor.tsx
+++ src/components/dialogs/EntityEditor/EntityEditor.tsx
@@ -19,29 +19,32 @@ const EntityEditor = () => {
   )?.data;
 
   const node = useImrStore((state) => state.imr.nodes[dialogData?.id || 0]);
-  const filters = node.filters;
+  const filters = node ? node.filters : undefined;
 
   return (
     <Dialog dialogName={DIALOG_NAME} className="sm:max-w-[40rem] w-fit">
-      <div className="relative h-full">
-        <Header name={node.display_name} />
-        <div className="flex-1 max-h-full overflow-hidden">
-          {filters.map((filter, index) => (
-            <FilterTypeSwitch
-              key={index}
-              filter={filter}
-              path={[index]}
-              nodeId={node.id}
-              pathString={`filters`}
-            />
-          ))}
+      {node && (
+        <div className="relative h-full">
+          <Header name={node.display_name} />
+          <div className="flex-1 max-h-full overflow-hidden">
+            {filters &&
+              filters.map((filter, index) => (
+                <FilterTypeSwitch
+                  key={index}
+                  filter={filter}
+                  path={[index]}
+                  nodeId={node.id}
+                  pathString={`filters`}
+                />
+              ))}
 
-          {filters.length === 0 && (
-            <AddBar pathString={`filters`} nodeId={node.id} path={[]} />
-          )}
-          <ApplyButton />
+            {filters && filters.length === 0 && (
+              <AddBar pathString={`filters`} nodeId={node.id} path={[]} />
+            )}
+            <ApplyButton />
+          </div>
         </div>
-      </div>
+      )}
     </Dialog>
   );
 };
