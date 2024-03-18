2c93dc4c3a54cddb7e6335dbbb7ed495f29427d2
tlmn <mail@tlmn.io>
2024-03-18 17:28:04 +0100

    add auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,28 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import jsonwebtoken from "jsonwebtoken";
+
+export const authOptions: NextAuthOptions = {
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+  },
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, session }) {
+      const jwt = jsonwebtoken.sign(token, process.env.NEXTAUTH_SECRET || "");
+      return { jwt, ...session };
+    },
+    async session({ session, token }) {
+      session.user = token;
+      return session;
+    },
+  },
+};
