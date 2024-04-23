c337bfe35f8a21cd4b73614a84fbdd189ff407ea
tlmn <mail@tlmn.io>
2024-04-23 19:37:09 +0200

    chore

--- src/components/map/TileLayer.tsx
+++ src/components/map/TileLayer.tsx
@@ -6,28 +6,26 @@ type Props = {
   layerType: "vector" | "satellite" | "osm";
 };
 
-const TileLayer = ({ layerType }: Props) => {
-  return (
-    <>
-      {layerType === "osm" && (
-        <LeafletTileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-      )}
+const TileLayer = ({ layerType }: Props) => (
+  <>
+    {layerType === "osm" && (
+      <LeafletTileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+    )}
 
-      {layerType === "vector" && (
-        <VectorTileLayer styleUrl={`/vectorStyles/style.json`} />
-      )}
+    {layerType === "vector" && (
+      <VectorTileLayer styleUrl={`/vectorStyles/style.json`} />
+    )}
 
-      {layerType === "satellite" && (
-        <LeafletTileLayer
-          url={`https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v12/tiles/{z}/{x}/{y}?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`}
-          attribution='© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        />
-      )}
-    </>
-  );
-};
+    {layerType === "satellite" && (
+      <LeafletTileLayer
+        url={`https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v12/tiles/{z}/{x}/{y}?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`}
+        attribution='© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      />
+    )}
+  </>
+);
 
 export default TileLayer;
