cb73d761ae03d744891f7301fdb83f03db9543ed
tlmn <mail@tlmn.io>
2024-04-23 19:35:46 +0200

    move TileLayer into separate component

--- src/components/map/MapContainer.tsx
+++ src/components/map/MapContainer.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { useRef } from "react";
-import { MapContainer as LeafletMapContainer, TileLayer } from "react-leaflet";
-import VectorTileLayer from "react-leaflet-vector-tile-layer";
+import { MapContainer as LeafletMapContainer } from "react-leaflet";
 
 import useMapStore from "@/stores/useMapStore";
 
@@ -13,8 +12,8 @@ import MapInterface from "./MapInterface";
 import MapKeyEvents from "./MapKeyEvents";
 import MapResizeHandler from "./MapResizeHandler";
 import GeoJSONResults from "./results/GeoJSONResults";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import SearchArea from "./results/SearchArea";
+import TileLayer from "./TileLayer";
 
 const MapContainer = () => {
   const mapContainerRef = useRef(null);
@@ -33,21 +32,7 @@ const MapContainer = () => {
           zoomControl={false}
         >
           <MapInterface />
-          {tilesLayer === "osm" && (
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-          )}
-          {tilesLayer === "vector" && (
-            <VectorTileLayer styleUrl={`/vectorStyles/style.json`} />
-          )}
-          {tilesLayer === "satellite" && (
-            <TileLayer
-              url={`https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v12/tiles/{z}/{x}/{y}?access_token=${process.env.NEXT_PUBLIC_MAPBOX_TOKEN}`}
-              attribution='© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            />
-          )}
+          <TileLayer layerType={tilesLayer} />
           <MapResizeHandler mapContainerRef={mapContainerRef} />
           <MapEvents />
           <MapKeyEvents />
