ffe8ede2b60509d2992012d723ae858c15a5bf1f
tlmn <mail@tlmn.io>
2024-05-13 10:58:06 +0200

    add credentials as temporary method

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -7,6 +8,23 @@ export const authOptions: NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_APP_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_APP_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { username, password } = credentials || {};
+        const user = { id: "1", name: "kid2", email: "kid2" };
+
+        if (username === "kid2" && password === "kid2") {
+          return user;
+        } else {
+          return null;
+        }
+      },
     }),
   ],
 };
