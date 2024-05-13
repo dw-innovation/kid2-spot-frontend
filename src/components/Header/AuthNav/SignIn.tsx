21791c76597339314b20d34533f669dca895b3a8
tlmn <mail@tlmn.io>
2024-05-13 10:52:25 +0200

    add icons

--- src/components/Header/AuthNav/SignIn.tsx
+++ src/components/Header/AuthNav/SignIn.tsx
@@ -1,7 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { LockOpen1Icon } from "@radix-ui/react-icons";
 import { signIn } from "next-auth/react";
 import React from "react";
 
-const SignIn = () => <Button onClick={() => signIn()}>sign in</Button>;
+const SignIn = () => {
+  return (
+    <Button onClick={() => signIn()}>
+      <LockOpen1Icon />
+      sign in
+    </Button>
+  );
+};
 
 export default SignIn;
