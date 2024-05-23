5dbc8d9e0a830b1836e52293937cf848558417a4
Tilman Miraß <tilman.mirass@dw.com>
2024-05-23 12:23:45 +0200

    remove unused import

--- src/components/map/MapControls/MapControls.tsx
+++ src/components/map/MapControls/MapControls.tsx
@@ -1,13 +1,10 @@
 "use client";
 
 import React from "react";
-import { useMap } from "react-leaflet";
 
 import ZoomControl from "./ZoomControl";
 
 const MapControls = () => {
-  const map = useMap();
-
   return (
     <div className="flex flex-col w-[10rem]">
       <ZoomControl />
