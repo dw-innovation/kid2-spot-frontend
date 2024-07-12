b22a5f27b422549469dc8b3ef5313a46a9c568eb
Tilman Miraß <tilman.mirass@dw.com>
2024-07-12 16:04:07 +0200

    set max and min zoom

--- src/components/map/MapControls/MapControls.tsx
+++ src/components/map/MapControls/MapControls.tsx
@@ -10,15 +10,25 @@ import ControlButton from "./ControlButton";
 const MapControls = () => {
   const map = useMap();
 
+  if (!map) {
+    return null;
+  }
+
   return (
     <div className="flex flex-col w-fit">
-      <ControlButton onClick={() => map.zoomOut()} className="rounded-b-none">
+      <ControlButton
+        onClick={() => map.getZoom() > 0 && map.zoomOut()}
+        className="rounded-b-none"
+      >
         <MinusIcon />
       </ControlButton>
 
       <ZoomControl />
 
-      <ControlButton onClick={() => map.zoomIn()} className="rounded-t-none">
+      <ControlButton
+        onClick={() => map.getZoom() < 19 && map.zoomIn()}
+        className="rounded-t-none"
+      >
         <PlusIcon />
       </ControlButton>
     </div>
