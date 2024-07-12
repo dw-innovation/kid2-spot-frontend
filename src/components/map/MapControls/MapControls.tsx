61fdbd73025bf7cc0c716682e4796f2461b2740a
Tilman Miraß <tilman.mirass@dw.com>
2024-07-12 15:49:11 +0200

    render ZoomControl

--- src/components/map/MapControls/MapControls.tsx
+++ src/components/map/MapControls/MapControls.tsx
@@ -4,21 +4,22 @@ import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 import React from "react";
 import { useMap } from "react-leaflet";
 
+import ZoomControl from "../ZoomControl";
 import ControlButton from "./ControlButton";
 
 const MapControls = () => {
   const map = useMap();
 
   return (
-    <div className="flex flex-col">
-      <ControlButton
-        onClick={() => map.zoomIn()}
-        className="rounded-b-none border-b-white border-b-[1px]"
-      >
-        <PlusIcon />
-      </ControlButton>
-      <ControlButton onClick={() => map.zoomOut()} className="rounded-t-none">
+    <div className="flex flex-col w-fit">
+      <ControlButton onClick={() => map.zoomOut()} className="rounded-b-none">
         <MinusIcon />
+      </ControlButton>
+
+      <ZoomControl />
+
+      <ControlButton onClick={() => map.zoomIn()} className="rounded-t-none">
+        <PlusIcon />
       </ControlButton>
     </div>
   );
