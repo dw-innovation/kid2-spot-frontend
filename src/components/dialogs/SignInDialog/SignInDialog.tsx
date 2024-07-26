f28bafe15266d43a744613f0035ddd9f1a9ff577
Tilman Miraß <tilman.mirass@dw.com>
2024-07-26 11:26:50 +0200

    style

--- src/components/dialogs/SignInDialog/SignInDialog.tsx
+++ src/components/dialogs/SignInDialog/SignInDialog.tsx
@@ -1,8 +1,10 @@
 import { signIn } from "next-auth/react";
 import React, { ChangeEvent, FormEvent, useState } from "react";
 
+import SpotLogo from "@/assets/icons/SpotLogo";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 import Dialog from "../Dialog";
 
@@ -48,34 +50,47 @@ const SignInDialog: React.FC = () => {
       preventClose={true}
     >
       <div className="flex items-center flex-col gap-2">
-        <h2 className="font-bold text-xl">Sign in with</h2>
+        <div className="flex flex-col items-start w-full gap-2 mb-4">
+          <SpotLogo />
+          <h2 className="pb-1 font-semibold leading-none text-center text-md font-inter text-muted-foreground ">
+            Geospatial search for OpenStreetMap
+          </h2>
+        </div>
         {PROVIDERS.map((provider) => (
           <Button
             onClick={() => signIn(provider.id)}
             className="w-full"
             key={provider.id}
           >
-            {provider.label}
+            Sign in with {provider.label}
           </Button>
         ))}
-        <form
-          onSubmit={handleSubmit}
-          className="flex items-center flex-col gap-2"
-        >
-          <Input
-            name="username"
-            placeholder="Username"
-            onChange={handleUserNameChange}
-            value={userName}
-          />
-          <Input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={handlePasswordChange}
-            value={password}
-          />
-          <Button type="submit">Sign in with credentials</Button>
+        <div className="flex items-center justify-center my-6 w-full">
+          <div className="flex-grow border-t border-[1px] border-gray-300"></div>
+          <span className="mx-4 text-gray-500 font-bold">or</span>
+          <div className="flex-grow border-t border-[1px] border-gray-300"></div>
+        </div>
+        <form onSubmit={handleSubmit} className="flex w-full flex-col gap-4">
+          <div className="flex w-full flex-col gap-2">
+            <Label className="text-gray-500 font-bold">Username</Label>
+            <Input
+              name="username"
+              placeholder="kid2"
+              onChange={handleUserNameChange}
+              value={userName}
+            />
+
+            <Label className="text-gray-500 font-bold">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="kid2"
+              onChange={handlePasswordChange}
+              value={password}
+            />
+          </div>
+
+          <Button type="submit">Sign in with Credentials</Button>
         </form>
       </div>
     </Dialog>
