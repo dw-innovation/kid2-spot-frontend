05105e8490ee0f835f146428bcab8ca78112d414
Tilman M <mail@tlmn.io>
2024-11-12 10:58:16 +0100

    adjust values

--- src/lib/const/const.ts
+++ src/lib/const/const.ts
@@ -1,9 +1,9 @@
 export const MINSIZES = [
-  { zoomLevel: 0, minArea: 5000000 }, // Very coarse detail at global level
-  { zoomLevel: 3, minArea: 1000000 }, // Continental view
-  { zoomLevel: 6, minArea: 100000 }, // Country-level view
-  { zoomLevel: 9, minArea: 5000 }, // Regional view
-  { zoomLevel: 12, minArea: 1000 }, // City view
-  { zoomLevel: 15, minArea: 100 }, // Neighborhood view
-  { zoomLevel: 18, minArea: 10 }, // Street-level view
+  { zoomLevel: 0, minArea: 50000000 }, // Very coarse detail at global level
+  { zoomLevel: 3, minArea: 10000000 }, // Continental view
+  { zoomLevel: 6, minArea: 1000000 }, // Country-level view
+  { zoomLevel: 9, minArea: 50000 }, // Regional view
+  { zoomLevel: 12, minArea: 10000 }, // City view
+  { zoomLevel: 15, minArea: 1000 }, // Neighborhood view
+  { zoomLevel: 18, minArea: 100 }, // Street-level view
 ];
