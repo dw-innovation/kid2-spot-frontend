134a283a334d682108a7fe9bccf781c25a9006f4
Tilman M <mail@tlmn.io>
2024-12-03 11:23:59 +0100

    inject session

--- src/components/FeedbackButton/FeedbackButton.tsx
+++ src/components/FeedbackButton/FeedbackButton.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 
 import FeedbackIcon from "@/assets/icons/FeedbackIcon";
+import useSaveSession from "@/lib/hooks/useSaveSession";
+
+import LoadingSpinner from "../LoadingSpinner";
 
 const FeedbackButton = () => {
   const [stripeWidth, setStripeWidth] = useState(0);
@@ -13,13 +16,33 @@ const FeedbackButton = () => {
     }
   }, [ref]);
 
+  const mutation = useSaveSession({
+    onSuccessCallbacks: [
+      (generatedSessionLink) => {
+        const feedbackUrlTemplate = process.env.NEXT_PUBLIC_FEEDBACK_URL || "";
+        const version = process.env.NEXT_PUBLIC_VERSION || "unknown";
+
+        const feedbackUrl = feedbackUrlTemplate
+          .replace("{{VERSION}}", version)
+          .replace(
+            "{{SESSION_LINK}}",
+            encodeURIComponent(generatedSessionLink)
+          );
+
+        window.open(feedbackUrl, "_blank", "noopener noreferrer");
+      },
+    ],
+  });
+
+  const handleClick = () => {
+    mutation.mutate();
+  };
+
   return (
-    <>
-      <a
-        href={process.env.NEXT_PUBLIC_FEEDBACK_URL}
-        target="_blank"
+    <div>
+      <button
+        onClick={handleClick}
         className="flex items-center gap-1 text-white bg-blue-500 hover:brighter p-2 font-noto cursor-pointer fixed right-0 bottom-[12.5%]"
-        rel="noopener noreferrer"
         style={{
           transform: `translateX(${hover ? 0 : stripeWidth + 8}px)`,
           zIndex: 100000,
@@ -28,11 +51,12 @@ const FeedbackButton = () => {
         onMouseOut={() => setHover(false)}
       >
         <div className="pr-1">
-          <FeedbackIcon />
+          {mutation.isPending ? <LoadingSpinner /> : <FeedbackIcon />}
         </div>
         <span ref={ref}>send feedback</span>
-      </a>
-    </>
+      </button>
+    </div>
   );
 };
+
 export default FeedbackButton;
