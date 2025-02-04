6c071eb7a63257e7662a4ec32c9e5be49d250780
Tilman M <mail@tlmn.io>
2025-02-04 11:33:18 +0100

    remove attribution

--- src/components/map/MapInterface/MapInterface.tsx
+++ src/components/map/MapInterface/MapInterface.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useMap } from "react-leaflet";
 
 import AddressSearchBox from "@/components/addressSearch";
 import Footer from "@/components/Footer";
@@ -10,42 +11,50 @@ import MapLegend from "@/components/map/MapLegend";
 import QueryPanel from "@/components/QueryPanel";
 import SearchCurrentViewButton from "@/components/SearchCurrentViewButton";
 
-const MapInterface = () => (
-  <>
-    <div className="absolute top-0 left-0 z-[400] flex items-start justify-between !font-sans p-2">
-      <QueryPanel />
-    </div>
+const MapInterface = () => {
+  const map = useMap();
 
-    <div className="absolute top-0 left-0 z-[400] flex items-center justify-center w-full p-2">
-      <SearchCurrentViewButton />
-    </div>
+  useEffect(() => {
+    map.attributionControl.setPrefix(false);
+  }, []);
 
-    <div className="absolute top-0 right-0 z-[400] flex items-start justify-between !font-sans p-2 w-fit">
-      <div className="flex flex-col-reverse items-end gap-2 font-sans text-base">
-        <div className="flex gap-2 flex-col items-end">
-          <AddressSearchBox />
-          <div className="flex gap-2 justify-end">
-            <div className="flex flex-col items-end gap-2">
-              <PolygonOutsideAlert />
-              <ResultsOutsideAlert />
+  return (
+    <>
+      <div className="absolute top-0 left-0 z-[400] flex items-start justify-between !font-sans p-2">
+        <QueryPanel />
+      </div>
+
+      <div className="absolute top-0 left-0 z-[400] flex items-center justify-center w-full p-2">
+        <SearchCurrentViewButton />
+      </div>
+
+      <div className="absolute top-0 right-0 z-[400] flex items-start justify-between !font-sans p-2 w-fit">
+        <div className="flex flex-col-reverse items-end gap-2 font-sans text-base">
+          <div className="flex gap-2 flex-col items-end">
+            <AddressSearchBox />
+            <div className="flex gap-2 justify-end">
+              <div className="flex flex-col items-end gap-2">
+                <PolygonOutsideAlert />
+                <ResultsOutsideAlert />
+              </div>
+              <MapControls />
             </div>
-            <MapControls />
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="absolute bottom-0 left-0 z-[400] flex items-end gap-2 justify-between p-2">
-      <MapLegend />
-      <div className="flex gap-1">
-        <LayerSelector />
+      <div className="absolute bottom-0 left-0 z-[400] flex items-end gap-2 justify-between p-2">
+        <MapLegend />
+        <div className="flex gap-1">
+          <LayerSelector />
+        </div>
       </div>
-    </div>
 
-    <div className="absolute bottom-0 right-0 z-[400] p-2">
-      <Footer />
-    </div>
-  </>
-);
+      <div className="absolute bottom-0 right-0 z-[400] p-2">
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 export default MapInterface;
