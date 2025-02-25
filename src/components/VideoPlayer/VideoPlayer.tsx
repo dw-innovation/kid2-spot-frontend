552595badf30a2b465ae8397b0b864296311c248
Tilman M <mail@tlmn.io>
2025-02-25 11:08:28 +0100

    half window size

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -29,26 +29,26 @@ const VideoPlayer = ({ videoUrl, posterUrl }: Props) => {
             <img
               src={posterUrl}
               alt="preview image of the video"
-              className="w-full"
+              className="w-1/2"
               style={{
                 filter: "blur(4px)",
               }}
             />
-            <div className="absolute z-10 h-full w-full flex items-center justify-center top-0 left-0">
+            <div className="absolute z-10 h-full w-1/2 flex items-center justify-center top-0 left-0">
               <Button onClick={handleClick}>
                 I agree with YouTube privacy statement, please show the video
               </Button>
             </div>
           </div>
         ) : (
-          <div className="w-full aspect-video relative">
+          <div className="w-1/2 aspect-video relative">
             <iframe
               src={videoUrl}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
-              className="w-full h-full rounded-lg shadow-lg"
+              className="w-1/2 h-full rounded-lg shadow-lg"
             ></iframe>
           </div>
         )}
