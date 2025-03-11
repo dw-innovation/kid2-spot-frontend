7154473d3a1334522124363cb1a585aa77b5481b
Tilman M <mail@tlmn.io>
2025-03-11 10:44:27 +0100

    enable maintenance

--- src/stores/useGlobalStore.ts
+++ src/stores/useGlobalStore.ts
@@ -102,7 +102,7 @@ const useGlobalStore = create<GlobalStoreInterface>((set) => ({
     })),
   dialogs: DIALOGS.map((dialog) => ({
     name: dialog,
-    isOpen: dialog === "inputStepper",
+    isOpen: dialog === "maintenance",
   })),
   toggleDialog: (name, isOpen) =>
     set((state) => ({ dialogs: toggleDialog(state.dialogs, name, isOpen) })),
