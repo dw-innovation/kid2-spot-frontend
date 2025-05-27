bfc2f680997f68a2bf3df1e7bec4d3432d5068e8
Tilman M <mail@tlmn.io>
2025-05-27 10:28:28 +0200

    update text

--- src/lib/const/strings/info.ts
+++ src/lib/const/strings/info.ts
@@ -8,126 +8,98 @@ export const STRINGS = {
   
   Spot was created by DW innovation and is co-funded by the German Secretary of State for Culture and the Media (BKM) via the KID2 Project (Künstliche Intelligenz gegen Desinformation #2)`,
   legalTitle: "Legal & Privacy Statement",
-  legal: `We strongly support the right to privacy. The Privacy Policy below explains all details on the anonymous user data we would like to process to learn from your usage of this application. Manage your consent to anonymous analytics on our site here:
-  
-  [[PRIVACY_MANAGER]]
-  
-  ## Legal Notice 
+  legal: `# Legal & Privacy Statement  
 
-Deutsche Welle | DW 
+[[PRIVACY_MANAGER]]
 
-represented by the Director General Peter Limbourg 
-##
-Kurt-Schumacher-Str. 3 
+We strongly support the right to privacy. The Privacy Policy below explains all details on the anonymous user data we would like to process to learn from your usage of this application. Manage your consent to anonymous analytics on our site here:  
 
-53113 Bonn
-##
-Phone: 0049 228 429-0 
+## Legal Notice  
 
-E-Mail: [info@dw.com](mailto:info@dw.com)
+Deutsche Welle | DW  
 
-VAT ID: DE123052407 
+represented by the Director General Peter Limbourg  
 
-## Attribution
+Kurt-Schumacher-Str. 3 53113 Bonn  
 
-SPOT utilizes Leaflet as its mapping library and OpenStreetMap (OSM) data for map visualization.
+Phone: 0049 228 429-0 E-Mail: [info@dw.com](mailto:info@dw.com)  
 
-## Privacy Policy 
+VAT ID: DE123052407  
 
-We strongly support the right to privacy. Protecting your personal data and treating it in a confidential way is something we take seriously. We'll process your personal data exclusively within the legal framework of the data protection provisions of the European Union, particularly the General Data Protection Regulation (hereinafter: GDPR). 
+## Privacy Policy  
 
-This data protection policy statement describes when, how and for which purposes we'll process your personal data. It also points out the rights you have regarding the processing of your data. In particular, you  will  find information on 
+We strongly support the right to privacy. Protecting your personal data and treating it in a confidential way is something we take seriously. We'll process your personal data exclusively within the legal framework of the data protection provisions of the European Union, particularly the General Data Protection Regulation (hereinafter: GDPR).  
 
--   who is legally responsible for the data processing (data controller) 
+This data protection policy statement describes when, how and for which purposes we'll process your personal data. It also points out the rights you have regarding the processing of your data. In particular, you will find information on  
 
--   the data collection and logging we carry out automatically when you use our application 
+-   who is legally responsible for the data processing (data controller)  
+-   the data collection and logging we carry out automatically when you use our application  
+-   the web analytics tool (e.g. Matomo) we have implemented to analyse and optimise the usage of our application  
+-   your rights as a data subject  
+-   your right to lodge a complaint  
 
--   the web analytics tool (e.g. Matomo) we have implemented to analyse and optimise  the usage of our application 
+1\. Responsible data controller  
 
--   your rights as a data subject 
+You can contact DW's data protection officer and data protection supervision at:  
 
--   your right to lodge a complaint 
+Deutsche Welle  
+Kurt-Schumacher-Strasse 3  
+53113 Bonn  
+T. +49.228.429-0  
 
-1\. Responsible data controller 
+[datenschutz@dw.com](mailto:datenschutz@dw.com)  
 
-You can contact DW's data protection officer and data protection supervision at: 
+2\. Data collection and logging  
 
-Deutsche Welle\
-Kurt-Schumacher-Strasse 3\
-53113 Bonn\
-T. +49.228.429-0 
+When you access our application on your device, we process the following anonymised data:  
 
-<datenschutz@dw.com> 
+-   the date and time of access  
+-   the duration of your visit  
+-   the amount of data transferred  
+-   your operating system  
+-   the device you have used for access  
+-   your browser type (and version)  
+-   the referrer (name of the website you accessed immediately before)  
+-   your anonymised IP address  
+-   your search query and the according system output  
 
-2\. Data collection and logging 
+We process this data on the basis of GDPR Article 6 (1) point f (legitimate interest), as they are required for us to provide the service, ensure technical operation, and investigate/remove malfunctions. It's in our interest to ensure the use and technical operability of our application. This data is automatically processed when our application is accessed. We usually erase the data after 30 days unless, under exceptional circumstances, we need them for a longer period for the above-mentioned purposes. In such a case, we erase the data as soon as they're no longer required.  
 
-When you access our application on your device, we process the following anonymised data: 
+3\. Web analytics (Matomo)  
 
--   the date and time of access 
+This application uses Matomo web analytics and makes use of cookieless tracking. Cookieless tracking is an alternative form of tracking that uses methods such as counting the number of unique, anonymised IP addresses or browser fingerprinting to identify users instead of cookies. This means that this application can still provide a smooth interaction and be analysed and improved based on the tracking of users without having to bother you with cookies. More information can be found here.  
 
--   the duration of your visit 
+4\. Third party services  
 
--   the amount of data transferred 
+This application uses third party services. Some of these services also need to process your search prompt to make the application work. When using the services, traffic data (IP address and timestamp) is being submitted, except for MongoDB. These services are:  
 
--   your operating system 
+-   **Nominatim** – We use Nominatim (Bayreuther Str. 29, 01187 Dresden, Germany) to geocode the selected area in your interface (‘search current area’). Nominatim does not process or record any personal data from using the SPOT application. Nominatim is an open-source tool.  
+-   **Maptiler** – We use Maptiler to geocode an address entered in the address search box. Maptiler does not process or record any personal data from using the SPOT application.  
+-   **Mapbox** – We use Mapbox to render satellite tiles in the ‘hybrid map’ mode. Mapbox does not process or record any personal data from using the SPOT application.  
+-   **Versatiles** – We use Versatiles to render vector tiles in the ‘vector map’ mode. Versatiles does not process or record any personal data from using the SPOT application. Versatiles does not have a privacy policy.  
+-   **OSM Tiles** – We use OSM Tiles to render OSM style raster tiles in the ‘OSM style map’ mode. OSM Tiles does not process or record any personal data from using the SPOT application.  
+-   **Google Street View** – We use the Google Maps Platform to display a Google Street View of a location in the SPOT interface. Google Maps Platform does not process or record any personal data from using the SPOT application.  
+-   **MongoDB** – When opening the Google Street View we use MongoDB to store and load sessions for sharing via ‘actions - share session’. MongoDB does not process or record any personal data from using the SPOT application.  
 
--   the device you  have used for access 
+When sharing your session, all settings of your SPOT session are being stored to MongoDB. MongoDB does not process any personal data such as your IP address or other. When restoring a shared session that session data is retrieved from MongoDB.  
 
--   your browser type (and version) 
+-   **YouTube** – We use YouTube to distribute our SPOT tutorial videos. To watch the videos, you have to agree to the YouTube privacy statement.  
 
--   the referrer (name of the website you accessed immediately before) 
+5\. Your rights as a data subject  
 
--   your anonymised IP address 
+We do not process any non-anonymised data. However, as a data subject you are generally entitled to the following rights:  
 
--   your search query and the according system output 
+-   right to information: You have a right to access the data we have stored about you as a person.  
+-   right to rectification and erasure: You can require us to correct inaccurate data or – provided that the legal grounds are in place – to erase your data.  
+-   restriction of processing: Provided that the legal grounds are in place, you can require us to restrict the processing of your data.  
+-   data portability: If you’ve provided us with data on the basis of a contract or your consent, and as long as there are legal grounds, you can require us to send you the data you gave us in a structured, commonly used and machine-readable format, or you can require us to send your data to a different controller.  
+-   objection to data processing on the legal basis of “legitimate interest” under GDPR Article 6 (1) point f: If there are reasons arising from your specific situation, you are entitled to object to our processing of your data at any time, provided that such an objection has its legal basis in a “legitimate interest”. If you make use of your right to object, we shall discontinue the processing of your data, unless we can – within the parameters of the law – demonstrate compelling legitimate grounds for further processing, outweighing your own rights.  
+-   objection to cookies: You can also object to the use of cookies at any time.  
+-   revocation of consent: If you’ve given us your consent to the processing of your data, you can revoke the same at any time with future effect. This, however, doesn’t affect the legitimacy of processing your data until the date of revocation.  
 
-We process this data on the basis of GDPR Article 6 (1) point f (legitimate interest), as they are required for us to provide the service, ensure technical operation, and investigate/remove malfunctions. It's in our interest to ensure the use and technical operability of our application. This data is automatically processed when our application is accessed. We usually erase the dataafter 30 daysunless, under exceptional circumstances, we need them for a longer period for the above-mentioned purposes. In such a case, we erase the data as soon as they're no longer required. 
+To exercise any of the aforementioned rights, please use the contact details as provided in clause 1 of this privacy policy. When you do so, please make sure we can clearly identify you. If you have any questions about the processing of your personal data, your rights as a data subject or any consent you may have given, please feel free to contact us. We’ll provide further information, free of any charges.  
 
-3\. Web analytics (Matomo) 
-
-This application uses Matomo web analytics and makes use of cookieless tracking. Cookieless tracking is an alternative form of tracking that uses methods such as counting the number of unique, anonymised IP addresses or browser fingerprinting to identify users instead of cookies. This means that this application can still provide a smooth interaction and be analysed and improved based on the tracking of users without having to bother you with cookies. More information can be found [here](https://matomo.org/cookie-consent-banners/). 
-
-4\. Third party services 
-
-This application uses third party services. Some of these services also need to process your search prompt to make the application work.   When using the services traffic data (IP address and timestamp) is being submitted, except for MongoDB. These services are: 
-
-Nominatim, we use [Nominatim](https://nominatim.openstreetmap.org/ui/about.html) (Bayreuther Str. 29, 01187 Dresden, Germany)  to geocode the selected area in your interface ('search current area'). Nominatim does not process or record any personal data from using the SPOT application.   Nominatim is an open-source tool. [Nominatim's privacy policy](https://osmfoundation.org/wiki/Privacy_Policy). 
-
-Maptiler, we use Maptiler to geocode an address entered in the address search box. Maptiler does not process or record any personal data from using the SPOT application. [Maptiler's privacy policy](https://www.maptiler.com/privacy-policy/). 
-
-Mapbox, we use Mapbox to render satellite tiles in the 'hybrid map' mode. Mapbox does not process or record any personal data from using the SPOT application. Mapbox's privacy policy can be found at [https://www.mapbox.com/legal/privacy](https://www.mapbox.com/legal/privacy). 
-
-Versatiles, we use Versatiles to render vector tiles in the 'vector map' mode.  Versatiles does not process or record any personal data from using the SPOT application. [Versatiles](https://versatiles.org/)  does not have a privacy policy. 
-
-OSM Tiles, we use OSM Tiles to render  OSM style raster tiles in the 'OSM style map' mode. OSM Tiles does not process or record any personal data from using the SPOT application. [OSM Tiles' privacy policy](https://osmfoundation.org/wiki/Privacy_Policy). 
-
-Google Street View, we use the Google Maps Platform to display a  Google Street View of a location in the SPOT interface. Google  Maps Platform  does not process or record any personal data from using the SPOT application. Google Maps Platform Terms of Service can be found at [https://cloud.google.com/maps-platform/terms](https://cloud.google.com/maps-platform/terms). When opening the Google Street View  
-
-MongoDB, we use MongoDB to store and load sessions for sharing  via 'actions - share session'.  Mongo DB does not process or record any personal data from using the SPOT application. [MongoDB's privacy policy](https://www.mongodb.com/legal/privacy). 
-
-When sharing your session, all settings of your SPOT session are being stored to MongoDB. MongoDB does not process any personal data such as your IP address or other. When restoring a shared session that session data is retrieved from MongoDB. 
-
-5\. Your rights as a data subject 
-
-We do not process any non-anonymised data. However, as a data subject you are generally entitled to the following rights: 
-
--   right to information: You have a right to access the data we have stored about you as a person. 
-
--   right to rectification and erasure: You can require us to correct inaccurate data or – provided that the legal grounds are in place – to erase your data. 
-
--   restriction of processing: Provided that the legal grounds are in place, you can require us to restrict the processing of your data. 
-
--   data portability: If you've provided us with data on the basis of a contract or your consent, and as long as there are legal grounds, you can require us to send you the data you gave us in a structured, commonly used and machine-readable format, or you can require us to send your data to a different controller. 
-
--   objection to data processing on the legal basis of "legitimate interest" under GDPR Article 6 (1) point f: If there are reasons arising from your specific situation, you are entitled to object to our processing of your data at any time, provided that such an objection has its legal basis in a "legitimate interest". If you make use of your right to object, we shall discontinue the processing of your data, unless we can – within the parameters of the law – demonstrate compelling legitimate grounds for further processing, outweighing your own rights. 
-
--   objection to cookies: You can also object to the use of cookies at any time. 
-
--   revocation of consent: If you've given us your consent to the processing of your data, you can revoke the same at any time with future effect. This, however, doesn't affect the legitimacy of processing your data until the date of revocation. 
-
-To exercise any of the aforementioned rights, please use the contact details as provided in clause 1 of this privacy  policy. When you do so, please make sure we can clearly identify you. If you have any questions about the processing of your personal data, your rights as a data subject or any consent you may have given, please feel free to contact us. We'll provide further information, free of any charges. 
-
-5\. Your right to lodge a complaint 
+6\. Your right to lodge a complaint  
 
 Without prejudice to any other administrative or judicial remedy, you have the right to lodge a complaint with a supervisory authority, in particular in the Member State of your habitual residence, place of work, or place of the alleged infringement if you consider that the processing of personal data relating to you infringes the GDPR.`,
   taiTitle: "Trusted AI Principles",
