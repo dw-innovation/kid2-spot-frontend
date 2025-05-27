d4aac2542fbcd8029319cb668bea6f1af377ffc9
Tilman M <mail@tlmn.io>
2025-05-27 10:56:19 +0200

    render flag

--- src/components/QueryPanel/Entities/Entity.tsx
+++ src/components/QueryPanel/Entities/Entity.tsx
@@ -6,9 +6,10 @@ import useGlobalStore from "@/stores/useGlobalStore";
 type Props = {
   name: string;
   id: number;
+  type: "cluster" | "nwr";
 };
 
-const Entity = ({ name, id }: Props) => {
+const Entity = ({ name, id, type }: Props) => {
   const toggleDialog = useGlobalStore((state) => state.toggleDialog);
   const setDialogData = useGlobalStore((state) => state.setDialogData);
 
@@ -20,6 +21,9 @@ const Entity = ({ name, id }: Props) => {
   return (
     <div className="flex gap-1 capitalize rounded-md w-fit border-[1px] border-black overflow-clip">
       <span className="p-1 leading-tight">{name}</span>
+      {type === "cluster" && (
+        <span className="uppercase text-xs font-bold">cluster</span>
+      )}
       <div className="flex h-full">
         <span className="border-r-[1px] border-black " />
         <button
